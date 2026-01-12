90e11c29402caa498bf7e8b411c1212da4264535
Lee JeongHyeok <justin101470@gmail.com>
2026-01-12 18:03:14 +0900

    팩토리 패턴 활용

--- src/main/java/com/sprint/mission/discodeit/factory/ServiceFactory.java
+++ src/main/java/com/sprint/mission/discodeit/factory/ServiceFactory.java
@@ -1,4 +1,31 @@
 package com.sprint.mission.discodeit.factory;
 
+import com.sprint.mission.discodeit.service.*;
+import com.sprint.mission.discodeit.service.jcf.*;
+
 public class ServiceFactory {
+    private static final ServiceFactory instance = new ServiceFactory();
+    private final UserService userService;
+    private final ChannelService channelService;
+    private final MessageService messageService;
+
+    private ServiceFactory() {
+        this.userService = new JCFUserService();
+        this.channelService = new JCFChannelService();
+        this.messageService =
+                new JCFMessageService(userService, channelService);
+    }
+
+    public static ServiceFactory getInstance() {
+        return instance;
+    }
+    public UserService userService() {
+        return userService;
+    }
+    public ChannelService channelService() {
+        return channelService;
+    }
+    public MessageService messageService() {
+        return messageService;
+    }
 }
