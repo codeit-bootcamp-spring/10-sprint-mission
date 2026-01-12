1af4a1f4623efb919545edd78c2e0a5aa3e3b9d4
Lee JeongHyeok <justin101470@gmail.com>
2026-01-12 18:08:53 +0900

    유저 JCF 구현

--- src/main/java/com/sprint/mission/discodeit/service/jcf/JCFUserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/jcf/JCFUserService.java
@@ -1,4 +1,37 @@
 package com.sprint.mission.discodeit.service.jcf;
 
-public class JCFUserService {
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.service.UserService;
+
+import java.util.*;
+
+public class JCFUserService implements UserService {
+    private final Map<UUID, User> data;
+
+    public JCFUserService() {
+        this.data = new HashMap<>();
+    }
+
+    public User create(String username) {
+        User user = new User(username);
+        data.put(user.getId(), user);
+        return user;
+    }
+
+    public User findById(UUID userId) {
+        return data.get(userId);
+    }
+
+    public List<User> findAll() {
+        return new ArrayList<>(data.values());
+    }
+
+    public void update(UUID userId, String username) {
+        User user = data.get(userId);
+        user.updateUsername(username);
+    }
+
+    public void delete(UUID userId) {
+        data.remove(userId);
+    }
 }
