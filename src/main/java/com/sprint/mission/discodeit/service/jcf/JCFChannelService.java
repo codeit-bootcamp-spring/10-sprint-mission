7b79c0828166a6e20c7ca87a6728389b01e0a1f6
Lee JeongHyeok <justin101470@gmail.com>
2026-01-12 18:07:40 +0900

    채널 JCF 구현

--- src/main/java/com/sprint/mission/discodeit/service/jcf/JCFChannelService.java
+++ src/main/java/com/sprint/mission/discodeit/service/jcf/JCFChannelService.java
@@ -1,4 +1,38 @@
 package com.sprint.mission.discodeit.service.jcf;
 
-public class JCFChannelService {
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.service.ChannelService;
+
+import java.util.*;
+
+public class JCFChannelService implements ChannelService {
+
+    private final Map<UUID, Channel> data;
+
+    public JCFChannelService() {
+        this.data = new HashMap<>();
+    }
+
+    public Channel create(String name) {
+        Channel channel = new Channel(name);
+        data.put(channel.getId(), channel);
+        return channel;
+    }
+
+    public Channel findById(UUID channelId) {
+        return data.get(channelId);
+    }
+
+    public List<Channel> findAll() {
+        return new ArrayList<>(data.values());
+    }
+
+    public void update(UUID channelId, String name) {
+        Channel channel = data.get(channelId);
+        channel.updateName(name);
+    }
+
+    public void delete(UUID channelId) {
+        data.remove(channelId);
+    }
 }
