dcc09cdba4438f278608f37b8aefb5fb4a5d860f
JunSuHwang <wnstn9904@naver.com>
2026-01-12 10:49:17 +0900

    Fix Channel import

--- src/main/java/com/sprint/mission/discodeit/service/MessageService.java
+++ src/main/java/com/sprint/mission/discodeit/service/MessageService.java
@@ -1,9 +1,9 @@
 package com.sprint.mission.discodeit.service;
 
+import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.entity.Message;
 import com.sprint.mission.discodeit.entity.User;
 
-import java.nio.channels.Channel;
 import java.util.List;
 import java.util.UUID;
 
