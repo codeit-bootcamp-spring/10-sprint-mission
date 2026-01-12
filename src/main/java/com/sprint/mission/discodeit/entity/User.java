a86a8ca2976833e60f448effce692d0626eca372
Lee JeongHyeok <justin101470@gmail.com>
2026-01-12 18:02:23 +0900

    유저 엔티티 생성

--- src/main/java/com/sprint/mission/discodeit/entity/User.java
+++ src/main/java/com/sprint/mission/discodeit/entity/User.java
@@ -1,4 +1,26 @@
 package com.sprint.mission.discodeit.entity;
 
-public class User {
+public class User extends BaseEntity {
+    private String username;
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + getId() +
+                ", username='" + username + '\'' +
+                '}';
+    }
+
+    public void updateUsername(String username) {
+        this.username = username;
+        this.updatedAt = System.currentTimeMillis();
+    }
 }
