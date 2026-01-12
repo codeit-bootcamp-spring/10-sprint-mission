1091d173734c4c9e8b006d7740d6324c28d52996
Lee JeongHyeok <justin101470@gmail.com>
2026-01-12 18:00:58 +0900

    채널 엔티티 생성

--- src/main/java/com/sprint/mission/discodeit/entity/Channel.java
+++ src/main/java/com/sprint/mission/discodeit/entity/Channel.java
@@ -1,4 +1,28 @@
 package com.sprint.mission.discodeit.entity;
 
-public class Channel {
+public class Channel extends BaseEntity {
+
+    private String name;
+
+    public Channel(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Channel{" +
+                "id=" + getId() +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+
+    public void updateName(String name) {
+        this.name = name;
+        this.updatedAt = System.currentTimeMillis();
+    }
 }
