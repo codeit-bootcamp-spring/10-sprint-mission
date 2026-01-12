14487eb3666a0b2ab3d0903871b0b4ec823ab2a7
Lee JeongHyeok <justin101470@gmail.com>
2026-01-12 18:05:21 +0900

    채널 인터페이스 생성

--- src/main/java/com/sprint/mission/discodeit/service/ChannelService.java
+++ src/main/java/com/sprint/mission/discodeit/service/ChannelService.java
@@ -1,4 +1,14 @@
 package com.sprint.mission.discodeit.service;
 
-public class ChannelService {
+import com.sprint.mission.discodeit.entity.Channel;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ChannelService {
+    Channel create(String name);
+    Channel findById(UUID channelId);
+    List<Channel> findAll();
+    void update(UUID channelId, String name);
+    void delete(UUID channelId);
 }
