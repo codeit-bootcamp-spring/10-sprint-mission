e8ebeafdad48b9ebb9dab5addc0944df3e414716
parkngg <millco0803com@gmail.com>
2026-01-12 13:47:01 +0900

    feat: Message 엔티티 구현

--- src/main/java/com/sprint/mission/discodeit/entity/Message.java
+++ src/main/java/com/sprint/mission/discodeit/entity/Message.java
@@ -1,4 +1,34 @@
 package com.sprint.mission.discodeit.entity;
 
+import java.util.UUID;
+
 public class Message {
+    private final UUID id;
+    private String content;
+    private final UUID userId;
+    private final UUID channelId;
+    private final Long createdAt;
+    private Long updatedAt;
+
+    public Message(String content, UUID userId, UUID channelId) {
+        this.id = UUID.randomUUID();
+        this.createdAt = System.currentTimeMillis();
+        this.updatedAt = this.createdAt;
+        this.content = content;
+        this.userId = userId;
+        this.channelId = channelId;
+    }
+
+    public UUID getId() { return id; }
+    public String getContent() { return content; }
+    public UUID getUserId() { return userId; }
+    public UUID getChannelId() { return channelId; }
+    public Long getCreatedAt() { return createdAt; }
+    public Long getUpdatedAt() { return updatedAt; }
+
+    public void update(String content) {
+        this.content = content;
+        this.updatedAt = System.currentTimeMillis();
+    }
 }
+
