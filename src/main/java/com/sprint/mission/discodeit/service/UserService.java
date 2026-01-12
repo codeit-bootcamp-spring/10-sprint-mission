dcceba58e15e5de225522d94a926557a1bdf6b84
Lee JeongHyeok <justin101470@gmail.com>
2026-01-12 18:06:49 +0900

    유저 인터페이스 생성

--- src/main/java/com/sprint/mission/discodeit/service/UserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/UserService.java
@@ -1,4 +1,14 @@
 package com.sprint.mission.discodeit.service;
 
-public class UserService {
+import com.sprint.mission.discodeit.entity.User;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface UserService {
+    User create(String username);
+    User findById(UUID userId);
+    List<User> findAll();
+    void update(UUID userId, String username);
+    void delete(UUID userId);
 }
