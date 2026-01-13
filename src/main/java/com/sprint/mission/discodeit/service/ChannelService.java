154ac34906e36acaea1f67d59da2ef14ac317aa2
parkngg <millco0803com@gmail.com>
2026-01-13 10:09:26 +0900

    feat: ChannelService CRUD 구현

--- src/main/java/com/sprint/mission/discodeit/service/ChannelService.java
+++ src/main/java/com/sprint/mission/discodeit/service/ChannelService.java
@@ -1,4 +1,18 @@
 package com.sprint.mission.discodeit.service;
 
-public class ChannelService {
+import com.sprint.mission.discodeit.entity.Channel;
+import java.util.UUID;
+import java.util.List;
+
+
+public interface ChannelService {
+    void create(Channel channel);
+
+    Channel readById(UUID id);
+
+    List<Channel> readAll();
+
+    void update(Channel channel);
+
+    void delete(UUID id);
 }
--- src/main/java/com/sprint/mission/discodeit/service/jcf/JCFChannelService.java
+++ src/main/java/com/sprint/mission/discodeit/service/jcf/JCFChannelService.java
@@ -1,4 +1,48 @@
 package com.sprint.mission.discodeit.service.jcf;
 
-public class JCFChannelService {
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.service.ChannelService;
+
+import java.util.UUID;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+
+public class JCFChannelService implements ChannelService {
+    private final Map<UUID, Channel> data;
+
+    public JCFChannelService(){
+        this.data = new HashMap<>();
+    }
+
+    @Override
+    public void create(Channel channel){
+        data.put(channel.getId(), channel);
+    }
+
+    @Override
+    public Channel readById(UUID id){
+        return data.get(id);
+    }
+
+    @Override
+    public List<Channel> readAll(){
+        return new ArrayList<>(data.values());
+    }
+
+    @Override
+    public void update(Channel channel){
+        Channel existingChannel = data.get(channel.getId());
+
+        if (existingChannel != null){
+            existingChannel.update(channel.getName(), channel.getDescription());
+        }
+    }
+
+    @Override
+    public void delete(UUID id){
+        data.remove(id);
+    }
 }
