269907117c0165b6556a1c56c93c60e8b42b7312
JungH200000 <jhpaxk0604@gmail.com>
2026-01-13 19:21:07 +0900

    검증 메서드

--- src/main/java/com/sprint/mission/discodeit/validation/ValidationMethods.java
+++ src/main/java/com/sprint/mission/discodeit/validation/ValidationMethods.java
@@ -1,43 +1,113 @@
 package com.sprint.mission.discodeit.validation;
 
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.entity.Message;
 import com.sprint.mission.discodeit.entity.User;
 
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.UUID;
 
 public class ValidationMethods {
-    // User ID null 검증
+    // User ID `null` 검증
     public static void validateUserId(UUID userId) {
         Objects.requireNonNull(userId, "User ID가 null입니다.");
     }
 
-    // Channel ID null 검증
+    // Channel ID `null` 검증
     public static void validateChannelId(UUID channelId) {
         Objects.requireNonNull(channelId, "Channel ID가 null입니다.");
     }
 
-    // Message ID null 검증
+    // Message ID `null` 검증
     public static void validateMessageId(UUID messageId) {
         Objects.requireNonNull(messageId, "Message ID가 null입니다.");
     }
 
-    // String null or blank 검증
-    // User: email이나 password 등
-    // Channel: channelName 등
+    // requestId와 targetId가 동일한지 검증
+    public static void validateSameId(UUID requestId, UUID targetId) {
+        if (!requestId.equals(targetId)) {
+            throw new IllegalStateException("본인의 정보만 수정 가능합니다.");
+        }
+    }
+
+    // String `null` or `blank` 검증
+    // User: email이나 password, partialName 등
+    // Channel: channelName, partialChannelName 등
+    // Message: content 등
     public static void validateString(String str, String fieldName) {
         if (str == null || str.isBlank()) {
             throw new IllegalArgumentException(fieldName + "이(가) 입력되지 않았습니다.");
         }
     }
 
-    // 이메일 중복 확인
+    // 객체 null 검증
+    public static void validateObject(Object obj, String objectName) {
+        String message = objectName + "가 null 입니다.";
+        Objects.requireNonNull(obj, message);
+    }
+
+    // 빈 상자([]) 출력하지 않을 때
+    // userId로 요청한 데이터가 존재하는지 검증
+    public static void validateDataByUserId(Map<UUID, User> data, UUID id) {
+        if (!data.containsKey(id)) {
+            throw new NoSuchElementException("해당 userId를 가진 User가 존재하지 않습니다.");
+        }
+    }
+
+    // channelId로 요청한 데이터가 존재하는지 검증
+    public static void validateDataByChannelId(Map<UUID, Channel> data, UUID id) {
+        if (!data.containsKey(id)) {
+            throw new NoSuchElementException("해당 channelId를 가진 Channel이 존재하지 않습니다.");
+        }
+    }
+
+    // messageId로 요청한 데이터가 존재하는지 검증
+    public static void validateDataByMessageId(Map<UUID, Message> data, UUID id) {
+        if (!data.containsKey(id)) {
+            throw new NoSuchElementException("해당 messageId를 가진 Message가 존재하지 않습니다.");
+        }
+    }
+
+    // email 중복 확인
     public static void duplicateEmail(Map<UUID, User> data, String email) {
         data.values().stream()
                 .filter(user -> user.getEmail().equals(email))
                 .findAny()
                 .ifPresent(user -> {
-                    throw new IllegalStateException("동일한 이메일이 존재합니다");
+                    throw new IllegalStateException("동일한 email이 존재합니다");
                 });
+    }
+
+    // 이미 참여한 채널인지 검증
+    public static void validateAlreadyParticipation(UUID userId, Channel channel) {
+        Boolean validateAlreadyParticipation = channel.getChannelMembersList().stream()
+                .noneMatch(user -> user.getId().equals(userId));
+        // false -> 중복 존재 O
+        if (!validateAlreadyParticipation) {
+            throw new IllegalStateException("이미 참여한 channel입니다.");
+        }
+    }
+
+    // 존재하는 유저인지 확인
+    public static void existUser(User user) {
+        if (user == null) {
+            throw new NoSuchElementException("해당 사용자가 없습니다.");
+        }
+    }
+
+    // 존재하는 채널인지 확인
+    public static void existChannel(Channel channel) {
+        if (channel == null) {
+            throw new NoSuchElementException("해당 채널이 없습니다.");
+        }
+    }
+
+    // 존재하는 메세지인지 확인
+    public static void existMessage(Message message) {
+        if (message == null) {
+            throw new NoSuchElementException("해당 메세지가 없습니다.");
+        }
     }
 }
