b96a5f60425cf9cde0c3985db5388aa4c5370d06
JungH200000 <jhpaxk0604@gmail.com>
2026-01-13 19:21:17 +0900

    Test 코드 추가

---
+++ src/main/java/com/sprint/mission/discodeit/test/MessageTest.java
@@ -0,0 +1,61 @@
+package com.sprint.mission.discodeit.test;
+
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.service.jcf.JCFChannelService;
+import com.sprint.mission.discodeit.service.jcf.JCFMessageService;
+import com.sprint.mission.discodeit.service.jcf.JCFUserService;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class MessageTest {
+    public static void main(String[] args) {
+        Map<UUID, User> userRepo = new HashMap<>();
+        JCFUserService userService = new JCFUserService(userRepo);
+        System.out.println("===== user1, user2, user3 생성 =====");
+        User user1 = userService.createUser("park@gmail.com", "user1Nick", "user1Name", "1234", "20000000");
+        User user2 = userService.createUser("jung@gmail.com", "user2Nick", "user2Name", "1234", "20101010");
+        User user3 = userService.createUser("hyun@gmail.com", "user3Nick", "user3Name", "1234", "20101020");
+        System.out.println(userService);
+
+        Map<UUID, Channel> channelRepo = new HashMap<>();
+        JCFChannelService channelService = new JCFChannelService(channelRepo);
+        System.out.println("===== channel1, channel2 생성 =====");
+        Channel channel1 = channelService.createChannel(user1, false, "channel1General", "General Channel");
+        Channel channel2 = channelService.createChannel(user2, true, "channel2Private", "private Channel");
+        System.out.println(channelService);
+
+        System.out.println("===== user1 channel2에 참여 =====");
+        userService.joinChannel(user1.getId(), user1.getId(), channel2);
+        System.out.println(user1.getJoinChannelList());
+//        userService.joinChannel(user1.getId(), user1.getId(), channel2); // 참여 여부 테스트
+        System.out.println("===== user3 channel1에 참여 =====");
+        userService.joinChannel(user3.getId(), user3.getId(), channel1);
+        System.out.println(user3.getJoinChannelList());
+
+        Map<UUID, Message> messageRepo = new HashMap<>();
+        JCFMessageService messageService = new JCFMessageService(messageRepo);
+        System.out.println("===== Message1, 2, 3 생성 =====");
+        Message message1 = messageService.createMessage(channel1, user1, "channel1user1message1");
+        Message message2 = messageService.createMessage(channel2, user1, "channel2user1message2");
+        Message message3 = messageService.createMessage(channel1, user3, "channel1user3message3");
+        System.out.println(message1.getId());
+        System.out.println(message2.getId());
+        System.out.println(message3.getId());
+//        Message message4 = messageService.createMessage(channel2, user3, "channel1user1message1"); // 해당 채널에 참여하지 않은 author가 작성 시도
+
+//        System.out.println("===== 유저1가 가진 메시지 목록 =====");
+//        System.out.println(user1.getWriteMessageList());
+//        System.out.println("===== 채널1이 가진 메시지 목록 =====");
+//        System.out.println(channel1.getChannelMessagesList());
+
+        System.out.println("===== 특정 메시지 정보 읽기 =====");
+        System.out.println(messageService.readMessageById(message1.getId()));
+
+        System.out.println("===== 모든 메시지 정보 읽기 =====");
+        System.out.println(messageService.readAllMessage());
+    }
+}
--- src/main/java/com/sprint/mission/discodeit/test/UserTest.java
+++ src/main/java/com/sprint/mission/discodeit/test/UserTest.java
@@ -1,88 +1,102 @@
 package com.sprint.mission.discodeit.test;
 
+import com.sprint.mission.discodeit.entity.BaseEntity;
 import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.service.jcf.JCFChannelService;
 import com.sprint.mission.discodeit.service.jcf.JCFUserService;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 public class UserTest {
     public static void main(String[] args) {
-        JCFUserService userService = new JCFUserService();
+        Map<UUID, User> userRepo = new HashMap<>();
+        JCFUserService userService = new JCFUserService(userRepo);
         System.out.println("===== user1, user2, user3 생성 =====");
-        User user1 = userService.createUser("park@gmail.com", "parkhyun", "박", "1234", "20000000");
-        User user2 = userService.createUser("jung@gmail.com", "jung", "정", "1234", "20101010");
-        User user3 = userService.createUser("hyun@gmail.com", "qorhvk", "hyun", "1234", "20101020");
+        User user1 = userService.createUser("park@gmail.com", "user1Nick", "user1Name", "1234", "20000000");
+        User user2 = userService.createUser("jung@gmail.com", "user2Nick", "user2Name", "1234", "20101010");
+        User user3 = userService.createUser("hyun@gmail.com", "user3Nick", "user3Name", "1234", "20101020");
         System.out.println(userService);
 
-//        System.out.println("===== 중복 이메일이 존재 =====");
-//        userService.createUser("park@gmail.com", "hyun", "현", "1234", "20201010");
-         // Exception in thread "main" java.lang.IllegalStateException: 동일한 이메일이 존재합니다
+        Map<UUID, Channel> channelRepo = new HashMap<>();
+        JCFChannelService channelService = new JCFChannelService(channelRepo);
+        System.out.println("===== channel1, channel2 생성 =====");
+        Channel channel1 = channelService.createChannel(user1, false, "channel1General", "General Channel");
+        Channel channel2 = channelService.createChannel(user2, true, "channel2Private", "private Channel");
+        System.out.println(channelService);
+
+        System.out.println("===== 채널 참여 =====");
+        System.out.println("===== user1 channel2에 참여 =====");
+        userService.joinChannel(user1.getId(), user1.getId(), channel2);
+//        userService.joinChannel(user1.getId(), channel2); // 참여 여부 테스트
+        System.out.println("===== user3 channel1에 참여 =====");
+        userService.joinChannel(user3.getId(), user3.getId(), channel1);
+
+        System.out.println("===== 채널 참여 =====");
+        user3.getJoinChannelList().stream().map(BaseEntity::getId).forEach(System.out::println);
+        System.out.println();
+        System.out.println(userService.joinChannel(user3.getId(), user3.getId(), channel2));
+        user3.getJoinChannelList().stream().map(BaseEntity::getId).forEach(System.out::println);
+
+        System.out.println("===== 채널 탈퇴 =====");
+//        userService.leaveChannel(user3.getId(), user3.getId(), channel1);
+//        user3.getJoinChannelList().stream().map(BaseEntity::getId).forEach(System.out::println);
 
 //        System.out.println("===== 로그인 =====");
-//        Optional<User> login = userService.readUserByEmailAndPw("jung@gmail.com", "1234");
-//        System.out.println(login);
-//
+
 //        System.out.println("===== 본인 정보 =====");
-//        Optional<User> findUser = userService.readUserById(null); // user1.getId()
-//        System.out.println(findUser);
 
 //        System.out.println("===== 모든 사용자 목록 =====");
-//        List<User> allUsers = userService.readAllUsers();
-//        System.out.println(allUsers);
-//
+
 //        System.out.println("===== 전체 검색으로 특정 사용자 목록 =====");
-//        List<User> searchUsers = userService.searchAllUsersByPartialName("hyun");
-//        System.out.println(searchUsers);
-//
-//
+
 //        System.out.println("===== 이메일 수정 =====");
-//        Optional<User> updateEmail = userService.updateEmail(user2.getId(),"으아아아아아@gmail.com");
+//        System.out.println(user1.getEmail());
+//        userService.updateEmail(user1.getId(), user1.getId(), "change@change.com");
+//        System.out.println(user1.getEmail());
+//
 //        System.out.println("===== 비밀번호 수정 =====");
-//        Optional<User> updatePassword = userService.updatePassword(user2.getId(), "5678");
+//        System.out.println(user1.getPassword());
+//        userService.updatePassword(user1.getId(), user1.getId(), "5678");
+//        System.out.println(user1.getPassword());
+//
 //        System.out.println("===== 별명 수정 =====");
-//        Optional<User> updateNickName = userService.updateNickName(user2.getId(), "배고파");
+//        System.out.println(user1.getNickName());
+//        userService.updateNickName(user1.getId(), user1.getId(), "changeNick1");
+//        System.out.println(user1.getNickName());
+//
 //        System.out.println("===== 사용자 이름 수정 =====");
-//        Optional<User> updateUserName = userService.updateUserName(user1.getId(), "배아파");
+//        System.out.println(user2.getUserName());
+//        userService.updateUserName(user2.getId(), user2.getId(), "changeName2");
+//        System.out.println(user2.getUserName());
+//
 //        System.out.println("===== 생년월일 수정 =====");
-//        Optional<User> updateBirthday = userService.updateBirthday(user2.getId(), "2000-01-01");
-//
-//        System.out.println(userService);
-//
+//        System.out.println(user3.getBirthday());
+//        userService.updateBirthday(user3.getId(), user3.getId(), "20000002");
+//        System.out.println(user3.getBirthday());
+
+        System.out.println("===== 삭제 =====");
+        System.out.println(userService.readUserById(user1.getId()));
+        System.out.println(user1.getOwnerChannelList());
+        System.out.println(user1.getJoinChannelList());
+        System.out.println(userService.readUserJoinChannelsByUserId(user1.getId()));
+        System.out.println("=====");
+//        user1.getJoinChannelList().forEach(channel -> channel.removeChannelMembers(user1));
+//        user1.getOwnerChannelList().forEach(channel->channel.re)
+        userService.deleteUser(user1.getId(), user2.getId());
+        System.out.println("=====");
+        System.out.println(channel1.getOwner());
+        System.out.println(channel1.getChannelMembersList());
+
 //        System.out.println("===== 전체 검색으로 특정 사용자 목록 =====");
-//        List<User> searchUsers1 = userService.searchAllUsersByPartialName("배");
-//        System.out.println(searchUsers1);
-//
-//        System.out.println("===== channel1, channel2 생성 =====");
-//        JCFChannelService channelService = new JCFChannelService();
-//        Channel channel1 = channelService.createChannel(user1, false, "general", "General Channel");
-//        Channel channel2 = channelService.createChannel(user2, true, "private", "private Channel");
-//        System.out.println(channelService);
-//        System.out.println(channel1);
-//        System.out.println(channel2);
-//
-//        System.out.println("===== channel1에 user2 참가 =====");
-//        userService.joinChannel(user2.getId(), channel1);
-//        userService.joinChannel(user3.getId(), channel1);
-//        System.out.println(user2.getJoinChannelList());
-//        System.out.println(channel1.getChannelMembersList());
-//        System.out.println(userService);
-//
+
 //        System.out.println("===== user2가 참여한 모든 채널 =====");
-//        List<Channel> allUsersInChannel = userService.readAllJoinChannelsAtUserByUserId(user2.getId());
-//
-//        System.out.println(user2.getJoinChannelList());
-//
-//        System.out.println("===== user2 참여 채널 탈퇴 =====");
-//        Optional<User> leaveUser = userService.leaveChannel(user2.getId(), channel1);
-//
-//        System.out.println(allUsersInChannel);
-//
-//        System.out.println("===== 삭제 =====");
-//        userService.deleteUser(user1.getId());
-//        userService.deleteUser(user2.getId());
-//        System.out.println(userService);
+
+//        System.out.println("===== 특정 사용자가 참여한 채널 중에서 특정 채널 검색 =====");
+
+//        System.out.println("===== 특정 사용자가 owner인 모든 채널 =====");
+
+
+
     }
 }
