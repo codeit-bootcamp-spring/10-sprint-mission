3e4ad0e0e23e9b60ac00428ed5f20de44b5e1e6c
JunCH163 <ckdgus163@gmail.com>
2026-01-13 11:43:31 +0900

    feat: Message 예외 처리 추가

--- src/main/java/com/sprint/mission/discodeit/service/jcf/JCFMessageService.java
+++ src/main/java/com/sprint/mission/discodeit/service/jcf/JCFMessageService.java
@@ -17,7 +17,9 @@ public class JCFMessageService implements MessageService {
 
     @Override
     public Message createMessage(String content, UUID channelId, UUID userId) {
-        Objects.requireNonNull(content, "content는 null이 될 수 없습니다.");
+        validationMessage(content);
+        Objects.requireNonNull(channelId);
+        Objects.requireNonNull(userId);
         Message message = new Message(content, channelId, userId);
         list.add(message);
         return message;
@@ -25,6 +27,7 @@ public class JCFMessageService implements MessageService {
 
     @Override
     public Message readMessage(UUID id) {
+        Objects.requireNonNull(id);
         for (Message message : list) {
             if (id.equals(message.getId())){
                 return message;
@@ -40,20 +43,31 @@ public class JCFMessageService implements MessageService {
 
     @Override
     public void updateMessage(UUID id, String content) {
+        Objects.requireNonNull(id);
+        validationMessage(content);
         readMessage(id).updateContent(content);
     }
 
     public void deleteMessage(UUID id) {
+        Objects.requireNonNull(id);
         list.remove(readMessage(id));
     }
 
     public boolean isMessageDeleted(UUID id) {
+        Objects.requireNonNull(id);
         for (Message message : list) {
             if(id.equals(message.getId())) {
                 return false;
             }
         }
         return true;
+    }
+
+    private void validationMessage(String content) {
+        Objects.requireNonNull(content, "content는 null이 될 수 없습니다.");
+        if(content.isBlank()) {
+            throw new IllegalArgumentException("content에 공백을 입력할 수 없습니다.");
+        }
     }
 
 }
