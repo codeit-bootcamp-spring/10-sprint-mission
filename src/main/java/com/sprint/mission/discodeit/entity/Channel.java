1d36dd3e678ccfa11c67188e21550c4a26ba5a54
LeeDyol <dlekthf819203@naver.com>
2026-01-15 15:33:41 +0900

    feat: User 엔티티 내 메시지 연관 관계 필드 및 관리 메서드 추가

--- src/main/java/com/sprint/mission/discodeit/entity/Channel.java
+++ src/main/java/com/sprint/mission/discodeit/entity/Channel.java
@@ -7,24 +7,36 @@ import java.util.UUID;
 public class Channel extends BaseEntity {
     private String channelName;         // 채널 이름 (변경 가능)
     private User user;                  // 채널 소유자 (변경 불가능)
-    private List<User> members;      // 채널 참가자 (변경 가능)
+    private List<User> members;         // 채널 참가자 (변경 가능)
     private ChannelType type;           // CHAT, VOICE (변경 불가능)
+
+    private List<Message> messages;     // 채널에서 발송된 메시지 목록
 
     public Channel(String channelName, User user, ChannelType channelType) {
         this.members = new ArrayList<>();
+        this.messages = new ArrayList<>();
 
         this.id = UUID.randomUUID();
         this.createdAt = System.currentTimeMillis();
         this.updatedAt = System.currentTimeMillis();
         this.channelName = channelName;
         this.user = user;
-        this.members.add(user);
         this.type = channelType;
+
+        this.members.add(user);
     }
 
     public void updateChannelName(String channelName) {
         this.channelName = channelName;
         this.updatedAt = System.currentTimeMillis();
+    }
+
+    public void addMessage(Message message) {
+        this.messages.add(message);
+    }
+
+    public void removeMessage(Message message) {
+        this.messages.remove(message);
     }
 
     public UUID getId() {
@@ -43,7 +55,7 @@ public class Channel extends BaseEntity {
         return channelName;
     }
 
-    public User getuser() {
+    public User getUser() {
         return user;
     }
 
@@ -53,5 +65,9 @@ public class Channel extends BaseEntity {
 
     public ChannelType getChannelType() {
         return type;
+    }
+
+    public List<Message> getMessages() {
+        return messages;
     }
 }
