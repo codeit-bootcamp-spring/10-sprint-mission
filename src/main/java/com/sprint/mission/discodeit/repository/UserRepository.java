410977eb3078ebc8810f8f36d01f1bac4bb9d5e9
kwaksss <kis2690@naver.com>
2026-01-20 18:19:03 +0900

    JCFUserRepository 완료

--- src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
@@ -9,5 +9,9 @@ import java.util.UUID;
 public interface UserRepository<K,V> {
 
     //"저장 로직" 과 관련된 기능을 도메인 모델 별 인터페이스로 선언하세요.
+    User save(User user); //create와 update를 save로 통합
+    User findById(UUID userId);
+    List<User> findAll();
+    void delete(UUID userId);
 
 }
