6e070c873e5ac266ae817066ac155cf44b2ff323
shong9124 <shifty393@gmail.com>
2026-01-20 13:22:18 +0900

    [feat] MessageRepository 구현

---
+++ src/main/java/com/sprint/mission/discodeit/repository/file/FileMessageRepository.java
@@ -0,0 +1,60 @@
+package com.sprint.mission.discodeit.repository.file;
+
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.repository.MessageRepository;
+
+import java.io.*;
+import java.util.*;
+
+public class FileMessageRepository implements MessageRepository {
+    private static final String FILE_PATH = "messages.ser";
+    private Map<UUID, Message> data;
+
+    public FileMessageRepository() {
+        this.data = load();
+    }
+
+    @Override
+    public Message save(Message message) {
+        data.put(message.getId(), message);
+        saveToFile();
+        return message;
+    }
+
+    @Override
+    public Optional<Message> findById(UUID messageId) {
+        return Optional.ofNullable(data.get(messageId));
+    }
+
+    @Override
+    public List<Message> findAll() {
+        return data.values().stream().toList();
+    }
+
+    @Override
+    public void deleteById(UUID messageId) {
+        data.remove(messageId);
+        saveToFile();
+    }
+
+    @SuppressWarnings("unchecked")
+    private Map<UUID, Message> load() {
+        File file = new File(FILE_PATH);
+        if (!file.exists()) {
+            return new HashMap<>();
+        }
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
+            return (Map<UUID, Message>) ois.readObject();
+        } catch (Exception e) {
+            throw new RuntimeException("메시지 데이터 로드 실패", e);
+        }
+    }
+
+    private void saveToFile() {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILE_PATH))) {
+            oos.writeObject(data);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFMessageRepository.java
@@ -0,0 +1,35 @@
+package com.sprint.mission.discodeit.repository.jcf;
+
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.repository.MessageRepository;
+
+import java.util.*;
+
+public class JCFMessageRepository implements MessageRepository {
+    private Map<UUID, Message> data;
+
+    public JCFMessageRepository() {
+        this.data = new HashMap<>();
+    }
+
+    @Override
+    public Message save(Message message) {
+        data.put(message.getId(), message);
+        return message;
+    }
+
+    @Override
+    public Optional<Message> findById(UUID messageId) {
+        return Optional.ofNullable(data.get(messageId));
+    }
+
+    @Override
+    public List<Message> findAll() {
+        return data.values().stream().toList();
+    }
+
+    @Override
+    public void deleteById(UUID messageId) {
+        data.remove(messageId);
+    }
+}
