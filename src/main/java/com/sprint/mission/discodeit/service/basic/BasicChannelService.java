153b4a682535564fafba4fae417ca3bc88bd8f6f
shong9124 <shifty393@gmail.com>
2026-01-20 15:59:07 +0900

    [feat] BasicService 구현

---
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicChannelService.java
@@ -0,0 +1,103 @@
+package com.sprint.mission.discodeit.service.basic;
+
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.ChannelRepository;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.service.ChannelService;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+import java.util.UUID;
+
+public class BasicChannelService implements ChannelService {
+    private final UserRepository userRepository;
+    private final ChannelRepository channelRepository;
+
+    public BasicChannelService(
+            UserRepository userRepository,
+            ChannelRepository channelRepository) {
+        this.userRepository = userRepository;
+        this.channelRepository = channelRepository;
+    }
+
+    @Override
+    public Channel createChannel(String channelName) {
+        return channelRepository.save(new Channel(channelName));
+    }
+
+    @Override
+    public List<Channel> getChannelList() {
+        return channelRepository.findAll();
+    }
+
+    @Override
+    public List<Channel> getChannelsByUser(UUID userId) {
+        findUserOrThrow(userId);
+
+        return channelRepository.findAll().stream()
+                .filter(channel -> channel.getJoinedUsers().stream()
+                        .anyMatch(user -> user.getId().equals(userId)))
+                .toList();
+    }
+
+    @Override
+    public Channel getChannelInfoById(UUID channelId) {
+        return findChannelOrThrow(channelId);
+    }
+
+    @Override
+    public Channel updateChannelName(UUID channelId, String newChannelName) {
+        Channel channel = findChannelOrThrow(channelId);
+        channel.updateChannelName(newChannelName);
+        channelRepository.save(channel);
+        return channel;
+    }
+
+    @Override
+    public void joinChannel(UUID channelId, UUID userId) {
+        Channel channel = findChannelOrThrow(channelId);
+        User user = findUserOrThrow(userId);
+        // 메모리 갱신
+        channel.addUser(user);
+        user.updateJoinedChannels(channel);
+        // file 갱신
+        channelRepository.save(channel);
+        userRepository.save(user);
+    }
+
+    @Override
+    public void leaveChannel(UUID channelId, UUID userId) {
+        Channel channel = findChannelOrThrow(channelId);
+        User user = findUserOrThrow(userId);
+
+        channel.removeUser(user.getId());
+        user.removeChannel(channel);
+
+        channelRepository.save(channel);
+        userRepository.save(user);
+    }
+
+    @Override
+    public void deleteChannel(UUID channelId) {
+        findChannelOrThrow(channelId);
+        channelRepository.deleteById(channelId);
+    }
+
+    private Channel findChannelOrThrow(UUID channelId) {
+        Objects.requireNonNull(channelId, "channelId는 null일 수 없습니다.");
+
+        return channelRepository.findById(channelId)
+                .orElseThrow(() ->
+                        new NoSuchElementException("해당 id를 가진 채널이 존재하지 않습니다."));
+    }
+
+    private User findUserOrThrow(UUID userId) {
+        Objects.requireNonNull(userId, "userId는 null값일 수 없습니다.");
+
+        return userRepository.findById(userId)
+                .orElseThrow(() ->
+                        new NoSuchElementException("해당 id를 가진 사용자가 존재하지 않습니다."));
+    }
+}
---
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserService.java
@@ -0,0 +1,69 @@
+package com.sprint.mission.discodeit.service.basic;
+
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.ChannelRepository;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.service.UserService;
+
+import java.util.*;
+
+public class BasicUserService implements UserService {
+    private final UserRepository userRepository;
+    private final ChannelRepository channelRepository;
+
+    public BasicUserService(
+            UserRepository userRepository,
+            ChannelRepository channelRepository
+    ) {
+        this.userRepository = userRepository;
+        this.channelRepository = channelRepository;
+    }
+
+    @Override
+    public User createUser(String username, String email, String password) {
+        return userRepository.save(new User(username, email, password));
+    }
+
+    @Override
+    public List<User> getUserList() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public List<User> getUsersByChannel(UUID channelId) {
+        if (channelRepository.findById(channelId).isEmpty()) {
+            throw new NoSuchElementException("해당 id를 가진 채널이 존재하지 않습니다.");
+        }
+
+        return userRepository.findAll().stream()
+                .filter(user -> user.getJoinedChannels().stream()
+                        .anyMatch(channel -> channel.getId().equals(channelId)))
+                .toList();
+    }
+
+    @Override
+    public User getUserInfoByUserId(UUID userId) {
+        return findUserInfoById(userId);
+    }
+
+    @Override
+    public User updateUserName(UUID userId, String newName) {
+        User user = findUserInfoById(userId);
+        user.updateUsername(newName);
+        userRepository.save(user);
+        return user;
+    }
+
+    @Override
+    public void deleteUser(UUID userId) {
+        findUserInfoById(userId);
+        userRepository.deleteById(userId);
+    }
+
+    private User findUserInfoById(UUID userId) {
+        Objects.requireNonNull(userId, "userId는 null 값일 수 없습니다.");
+
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new NoSuchElementException("해당 id를 가진 유저가 존재하지 않습니다."));
+    }
+}
