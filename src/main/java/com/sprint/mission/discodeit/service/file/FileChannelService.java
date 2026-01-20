a0673ff5f204564b52b9865f59ca79e12985c307
JunSuHwang <wnstn9904@naver.com>
2026-01-20 17:31:59 +0900

    add: 파일시스템 기반 ChannelService 구현체 추가

---
+++ src/main/java/com/sprint/mission/discodeit/service/file/FileChannelService.java
@@ -0,0 +1,147 @@
+package com.sprint.mission.discodeit.service.file;
+
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.entity.ChannelType;
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.service.ChannelService;
+import com.sprint.mission.discodeit.service.MessageService;
+import com.sprint.mission.discodeit.service.UserService;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class FileChannelService implements ChannelService {
+    private UserService userService;
+    private MessageService messageService;
+
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    public void setMessageService(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @Override
+    public Channel createChannel(String channelName, ChannelType channelType, String description) {
+        validateChannelExist(channelName);
+        Channel channel = new Channel(channelName, channelType, description);
+        save(channel);
+        return channel;
+    }
+
+    @Override
+    public Channel getChannel(UUID channelId) {
+        Path channelPath = getChannelPath(channelId);
+
+        if(!channelPath.toFile().exists()) {
+            throw new NoSuchElementException("해당 채널이 존재하지 않습니다.");
+        }
+        try (FileInputStream fis =  new FileInputStream(channelPath.toFile());
+                ObjectInputStream ois = new ObjectInputStream(fis)) {
+            return (Channel) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public List<Channel> getAllChannels() {
+        Path channelPath = Path.of("channels");
+        if(Files.exists(channelPath)) {
+            try {
+                List<Channel> channels = Files.list(channelPath)
+                        .map(path -> {
+                            try(
+                                    FileInputStream fis = new FileInputStream(path.toFile());
+                                    ObjectInputStream ois = new ObjectInputStream(fis)
+                            ) {
+                                Channel channel = (Channel) ois.readObject();
+                                return channel;
+                            } catch (IOException | ClassNotFoundException e) {
+                                throw new RuntimeException(e);
+                            }
+                        })
+                        .toList();
+                return channels;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            return new ArrayList<>();
+        }
+    }
+
+    @Override
+    public List<Channel> getChannelsByUserId(UUID userId) {
+        return userService.getUser(userId).getChannels();
+    }
+
+    @Override
+    public Channel updateChannel(UUID channelId, String channelName, ChannelType channelType, String description) {
+        validateChannelExist(channelName);
+        Channel updatechannel = getChannel(channelId);
+        Optional.ofNullable(channelName)
+                .ifPresent(updatechannel::updateChannelName);
+        Optional.ofNullable(channelType)
+                .ifPresent(updatechannel::updateChannelType);
+        Optional.ofNullable(description)
+                .ifPresent(updatechannel::updateDescription);
+        save(updatechannel);
+        return updatechannel;
+    }
+
+    @Override
+    public void deleteChannel(UUID channelId) {
+        Path channelPath = getChannelPath(channelId);
+        try {
+            Files.delete(channelPath);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void joinChannel(UUID channelId, UUID userId) {
+        Channel channel = getChannel(channelId);
+        User user = userService.getUser(userId);
+        channel.addUser(user);
+        user.addChannel(channel);
+        // save(channel);
+        // save(user);
+    }
+
+    @Override
+    public void leaveChannel(UUID channelId, UUID userId) {
+        Channel channel = getChannel(channelId);
+        User user = userService.getUser(userId);
+        channel.removeUser(user);
+        user.removeChannel(channel);
+        // save(channel);
+        // save(user);
+    }
+
+    private Path getChannelPath(UUID channelId) {
+        return Paths.get("channels", channelId.toString() + ".ser");
+    }
+
+    private void save(Channel channel) {
+        Path channelPath = getChannelPath(channel.getId());
+        try (
+                FileOutputStream fos = new FileOutputStream(channelPath.toFile());
+                ObjectOutputStream oos = new ObjectOutputStream(fos)
+        ) {
+            oos.writeObject(channel);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void validateChannelExist(String channelName) {
+        if(getAllChannels().stream().anyMatch(c -> c.getChannelName().equals(channelName)))
+            throw new IllegalStateException("이미 존재하는 채널 이름입니다.");
+    }
+}
