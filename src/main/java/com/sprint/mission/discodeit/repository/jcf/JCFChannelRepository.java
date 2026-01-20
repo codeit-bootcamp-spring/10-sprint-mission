cddfcff3cd773b725f4ab0f292a8b06c9e43f574
parkngg <millco0803com@gmail.com>
2026-01-21 04:40:45 +0900

    feat: 각 도메인별 JCF Repository 구현

--- src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFChannelRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFChannelRepository.java
@@ -1,4 +1,31 @@
 package com.sprint.mission.discodeit.repository.jcf;
 
-public class JCFChannelRepository {
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.repository.ChannelRepository;
+
+import java.util.*;
+
+public class JCFChannelRepository implements ChannelRepository {
+    private final Map<UUID, Channel> data = new HashMap<>();
+
+    @Override
+    public Channel save(Channel channel){
+        data.put(channel.getId(), channel);
+        return channel;
+    }
+
+    @Override
+    public Optional<Channel> findById(UUID id){
+        return Optional.ofNullable(data.get(id));
+    }
+
+    @Override
+    public List<Channel> findAll(){
+        return new ArrayList<>(data.values());
+    }
+
+    @Override
+    public void delete(Channel channel){
+        data.remove(channel.getId());
+    }
 }
--- src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFUserRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFUserRepository.java
@@ -1,4 +1,31 @@
 package com.sprint.mission.discodeit.repository.jcf;
 
-public class JCFUserRepository {
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.UserRepository;
+
+import java.util.*;
+
+public class JCFUserRepository implements UserRepository {
+    private final Map<UUID, User> data = new HashMap<>();
+
+    @Override
+    public User save(User user){
+        data.put(user.getId(), user);
+        return user;
+    }
+
+    @Override
+    public Optional<User> findById(UUID id){
+        return Optional.ofNullable(data.get(id));
+    }
+
+    @Override
+    public List<User> findAll(){
+        return new ArrayList<>(data.values());
+    }
+
+    @Override
+    public void delete(User user){
+        data.remove(user.getId());
+    }
 }
