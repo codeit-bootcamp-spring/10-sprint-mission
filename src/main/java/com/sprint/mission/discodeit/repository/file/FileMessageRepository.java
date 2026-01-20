7ef8a0ad4df4a4a6822db3307a06879c8cbe0892
parkngg <millco0803com@gmail.com>
2026-01-21 05:42:15 +0900

    feat: 각 도메인별 File Repository 구현 및 BaseFileRepository로 공통 로직 분리

---
+++ src/main/java/com/sprint/mission/discodeit/repository/file/FileMessageRepository.java
@@ -0,0 +1,37 @@
+package com.sprint.mission.discodeit.repository.file;
+
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.repository.MessageRepository;
+
+import java.util.*;
+
+public class FileMessageRepository extends BaseFileRepository<Message> implements MessageRepository {
+    public FileMessageRepository() {
+        super("messages.ser");
+    }
+
+    @Override
+    public Message save(Message message){
+        Map<UUID, Message> data = loadData();
+        data.put(message.getId(), message);
+        saveData(data);
+        return message;
+    }
+
+    @Override
+    public Optional<Message> findById(UUID id){
+        return Optional.ofNullable(loadData().get(id));
+    }
+
+    @Override
+    public List<Message> findAll(){
+        return new ArrayList<>(loadData().values());
+    }
+
+    @Override
+    public void delete(Message message){
+        Map<UUID, Message> data = loadData();
+        data.remove(message.getId());
+        saveData(data);
+    }
+}
