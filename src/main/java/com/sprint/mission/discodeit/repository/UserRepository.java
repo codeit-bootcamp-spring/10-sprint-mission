5d7ea697f6c0ab638f7ffa9309035cffa5e8d027
LeeDyol <dlekthf819203@naver.com>
2026-01-22 16:55:48 +0900

    feat: 도메인별 Repository 인터페이스 정의

---
+++ src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
@@ -0,0 +1,24 @@
+package com.sprint.mission.discodeit.repository;
+
+import com.sprint.mission.discodeit.entity.User;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository {
+    // 사용자 저장
+    void save(User user);
+
+    // 사용자 단건 조회
+    Optional<User> findById(UUID userId);
+
+    // 사용자 전체 조회
+    List<User> findAll();
+
+    // 사용자 삭제
+    void delete(User targetUser);
+
+    // 유효성 검사 (이메일 중복)
+    boolean existsByEmail(String email);
+}
