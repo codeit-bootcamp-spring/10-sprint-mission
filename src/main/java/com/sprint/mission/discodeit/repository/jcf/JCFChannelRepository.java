b76469ca94a374adcba8713944c79a6a4377a22d
LeeDyol <dlekthf819203@naver.com>
2026-01-22 17:03:22 +0900

    feat: JCF 기반 Repository 구현체 추가

---
+++ src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFChannelRepository.java
@@ -0,0 +1,43 @@
+package com.sprint.mission.discodeit.repository.jcf;
+
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.repository.ChannelRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class JCFChannelRepository implements ChannelRepository {
+    private final List<Channel> data;
+
+    public JCFChannelRepository() {
+        data = new ArrayList<>();
+    }
+
+    // 채널 저장
+    @Override
+    public void save(Channel channel) {
+        data.add(channel);
+    }
+
+    // 채널 단건 조회
+    @Override
+    public Optional<Channel> findById(UUID channelId) {
+        return data.stream()
+                .filter(channel -> channel.getId().equals(channelId))
+                .findFirst();
+    }
+
+    // 채널 전체 조회
+    @Override
+    public List<Channel> findAll() {
+        return data;
+    }
+
+    // 채널 삭제
+    @Override
+    public void delete(Channel channel) {
+        data.remove(channel);
+    }
+}
---
+++ src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFMessageRepository.java
@@ -0,0 +1,43 @@
+package com.sprint.mission.discodeit.repository.jcf;
+
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.repository.MessageRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class JCFMessageRepository implements MessageRepository {
+    private final List<Message> data;       // 모든 메시지
+
+    private JCFMessageRepository() {
+        data = new ArrayList<>();
+    }
+
+    // 메시지 저장
+    @Override
+    public void save(Message message) {
+        data.add(message);
+    }
+
+    // 메시지 단건 조회
+    @Override
+    public Optional<Message> findById(UUID messageId) {
+        return data.stream()
+                .filter(message -> message.getId().equals(messageId))
+                .findFirst();
+    }
+
+    // 메시지 전체 조회
+    @Override
+    public List<Message> findAll() {
+        return data;
+    }
+
+    // 메시지 삭제
+    @Override
+    public void delete(Message message) {
+        data.remove(message);
+    }
+}
