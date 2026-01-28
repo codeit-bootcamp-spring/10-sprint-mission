709a028d3ea91b3b22f635f28b19c6941a20544a
JungH200000 <jhpaxk0604@gmail.com>
2026-01-28 13:43:45 +0900

    refactor: 시간과 관련된 필드 타입을 `Instant`로 변경

--- src/main/java/com/sprint/mission/discodeit/entity/BaseEntity.java
+++ src/main/java/com/sprint/mission/discodeit/entity/BaseEntity.java
@@ -12,20 +12,20 @@ public abstract class BaseEntity implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
     protected final UUID id; // 객체 식별을 위한 id
-    protected final Long createdAt; // 객체 생성 시간(유닉스 타임스탬프)
-    protected Long updatedAt; // 객체 수정 시간(유닉스 타임스탬프)
+    protected final Instant createdAt; // 객체 생성 시간: "2026-01-28T00:52:05.985737500Z"
+    protected Instant updatedAt; // 객체 수정 시간
 
     protected BaseEntity() {
         // id 초기화
         this.id = UUID.randomUUID();
         // 시간 초기화
-        this.createdAt = Instant.now().toEpochMilli();
+        this.createdAt = Instant.now();
         this.updatedAt = this.createdAt;
     }
 
     // update 시간 메소드
     public void updateTime() {
-        this.updatedAt = Instant.now().toEpochMilli();
+        this.updatedAt = Instant.now();
     }
 
 }
