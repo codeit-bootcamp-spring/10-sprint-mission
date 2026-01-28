4984f4434274ae23696e04a531ab318a56b53270
PSG-00 <cdjsdj1902@gmail.com>
2026-01-28 09:46:13 +0900

    BinrayContent 도메인 추가

---
+++ src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
@@ -0,0 +1,27 @@
+package com.sprint.mission.discodeit.entity;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.UUID;
+
+public class BinaryContent implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private UUID id;
+    private Instant createAt;
+    //
+    private String fileName;
+    private String contentType;
+    private byte[] content;
+    private long size;
+
+    public BinaryContent(String fileName, String contentType, byte[] content) {
+        this.id = UUID.randomUUID();
+        this.createAt = Instant.now();
+        this.fileName = fileName;
+        this.contentType = contentType;
+        this.content = content;
+        this.size = (content != null) ? content.length : 0L;
+    }
+
+}
