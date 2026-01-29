e11d56b5071e3dd7b465a9df75b4734abc70723f
PSG-00 <cdjsdj1902@gmail.com>
2026-01-29 14:40:26 +0900

    매핑할 때 파생 데이터 명시

--- src/main/java/com/sprint/mission/discodeit/mapper/UserMapper.java
+++ src/main/java/com/sprint/mission/discodeit/mapper/UserMapper.java
@@ -3,9 +3,12 @@ package com.sprint.mission.discodeit.mapper;
 import com.sprint.mission.discodeit.dto.UserDto;
 import com.sprint.mission.discodeit.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+
+    @Mapping(target = "isOnline", source = "isOnline")
     UserDto.Response toResponse(User user, boolean isOnline);
 
 }
