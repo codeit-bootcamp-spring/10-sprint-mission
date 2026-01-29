2077456fe791875dbd5591113c617fd53d7d5e74
초시맥북 <fyg7575@gmail.com>
2026-01-30 00:30:57 +0900

    UserResponseMapper: 매퍼 어노테이션 적용 JCFRepository: BinaryContent, ReadStatus,UserStatus 구현

---
+++ src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFBinaryContentRepository.java
@@ -0,0 +1,40 @@
+package com.sprint.mission.discodeit.repository.jcf;
+
+import com.sprint.mission.discodeit.entity.BinaryContent;
+import com.sprint.mission.discodeit.entity.UserStatus;
+import com.sprint.mission.discodeit.repository.BinaryContentRepository;
+
+import java.util.*;
+
+public class JCFBinaryContentRepository implements BinaryContentRepository {
+    private final Map<UUID, BinaryContent> data;
+    public JCFBinaryContentRepository() {
+        this.data = new HashMap<>();
+    }
+
+    @Override
+    public BinaryContent save(BinaryContent binaryContent) {
+        data.put(binaryContent.getId(), binaryContent);
+        return binaryContent;
+    }
+
+    @Override
+    public Optional<BinaryContent> findById(UUID id) {
+        return Optional.ofNullable(data.get(id));
+    }
+
+    @Override
+    public List<BinaryContent> findAll() {
+        return this.data.values().stream().toList();
+    }
+
+    @Override
+    public boolean existsById(UUID id) {
+        return data.containsKey(id);
+    }
+
+    @Override
+    public void deleteById(UUID id) {
+        this.data.remove(id);
+    }
+}
