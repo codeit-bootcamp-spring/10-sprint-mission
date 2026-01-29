d703c5c094201fb29e251bf2b0c6e030fa1abab1
PSG-00 <cdjsdj1902@gmail.com>
2026-01-29 16:25:58 +0900

    feat: BinaryContentService 작성

---
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicBinaryContentService.java
@@ -0,0 +1,51 @@
+package com.sprint.mission.discodeit.service.basic;
+
+import com.sprint.mission.discodeit.dto.BinaryContentDto;
+import com.sprint.mission.discodeit.entity.BinaryContent;
+import com.sprint.mission.discodeit.repository.BinaryContentRepository;
+import com.sprint.mission.discodeit.service.BinaryContentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class BasicBinaryContentService implements BinaryContentService {
+    private final BinaryContentRepository binaryContentRepository;
+
+    @Override
+    public BinaryContent create(BinaryContentDto.CreateRequest request) {
+        String fileName = request.fileName();
+        String contentType = request.contentType();
+        byte[] content = request.content();
+
+        BinaryContent binaryContent = new BinaryContent(fileName, contentType, content);
+        return binaryContentRepository.save(binaryContent);
+    }
+
+    @Override
+    public BinaryContent find(UUID binaryContentId) {
+        return binaryContentRepository.findById(binaryContentId)
+                .orElseThrow(() -> new NoSuchElementException("해당 파일을 찾을 수 없습니다: " + binaryContentId));
+    }
+
+    @Override
+    public List<BinaryContent> findAllByIn(List<UUID> binaryContentIds) {
+        return binaryContentIds.stream()
+                .map(id -> binaryContentRepository.findById(id)
+                        .orElseThrow(() -> new NoSuchElementException("파일이 사라졌습니다: " + id)))
+                .toList();
+    }
+
+    @Override
+    public void delete(UUID binaryContentId) {
+        if (!binaryContentRepository.findById(binaryContentId).isPresent()) {
+            throw new NoSuchElementException("해당 파일을 찾을 수 없습니다: " + binaryContentId);
+        }
+
+        binaryContentRepository.deleteById(binaryContentId);
+    }
+}
