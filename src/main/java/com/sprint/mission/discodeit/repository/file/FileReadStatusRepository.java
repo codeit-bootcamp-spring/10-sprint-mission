31efa770806aed5ae7f75e5dc25dc4a4d689d84a
JungH200000 <jhpaxk0604@gmail.com>
2026-01-29 19:36:56 +0900

    feat: `FileReadStatusRepository` 구현

---
+++ src/main/java/com/sprint/mission/discodeit/repository/file/FileReadStatusRepository.java
@@ -0,0 +1,50 @@
+package com.sprint.mission.discodeit.repository.file;
+
+import com.sprint.mission.discodeit.entity.ReadStatus;
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.ReadStatusRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository
+public class FileReadStatusRepository implements ReadStatusRepository {
+    private final Map<UUID, ReadStatus> data;
+    private final FileObjectStore fileObjectStore;
+
+    public FileReadStatusRepository(FileObjectStore fileObjectStore) {
+        this.data = fileObjectStore.getReadStatusesData();
+        this.fileObjectStore = fileObjectStore;
+    }
+
+    @Override
+    public void save(ReadStatus readStatus) {
+        data.put(readStatus.getId(), readStatus);
+        fileObjectStore.saveData();
+    }
+
+    @Override
+    public Optional<ReadStatus> findById(UUID readStatusId) {
+        return Optional.ofNullable(data.get(readStatusId));
+    }
+
+    @Override
+    public List<ReadStatus> findAllByUserId(UUID userId) {
+        return data.values().stream()
+                .filter(readStatus -> readStatus.getUserId().equals(userId))
+                .toList();
+    }
+
+    @Override
+    public List<ReadStatus> findAllByChannelId(UUID channelId) {
+        return data.values().stream()
+                .filter(readStatus -> readStatus.getChannelId().equals(channelId))
+                .toList();
+    }
+
+    @Override
+    public void delete(UUID readStatusId) {
+        data.remove(readStatusId);
+        fileObjectStore.saveData();
+    }
+}
