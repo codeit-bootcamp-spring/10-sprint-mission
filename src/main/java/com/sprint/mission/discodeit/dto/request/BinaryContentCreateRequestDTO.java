c3029a2a76943c4794c646bc6a98727a501785d9
LeeDyol <dlekthf819203@naver.com>
2026-01-29 13:45:42 +0900

    feat: BinaryContent 엔티티 내 파일명(filename) 필드 추가

--- src/main/java/com/sprint/mission/discodeit/dto/request/BinaryContentCreateRequestDTO.java
+++ src/main/java/com/sprint/mission/discodeit/dto/request/BinaryContentCreateRequestDTO.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 
 @Getter
 public class BinaryContentCreateRequestDTO {
+    private String fileName;
     private byte[] binaryContent;
     private BinaryContentType binaryContentType;
 }
