28f1e8ad786241638629c522ab9117cca8f70c51
JungH200000 <jhpaxk0604@gmail.com>
2026-01-30 17:27:30 +0900

    feat: `Service` 고도화에 따른 `File*Repository` 리팩토링 및 구현으로 `JCF*Repository` 리팩토링 및 구현

---
+++ src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFReadStatusRepository.java
@@ -0,0 +1,58 @@
+package com.sprint.mission.discodeit.repository.jcf;
+
+import com.sprint.mission.discodeit.entity.ReadStatus;
+import com.sprint.mission.discodeit.repository.ReadStatusRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository
+public class JCFReadStatusRepository implements ReadStatusRepository {
+    private final Map<UUID, ReadStatus> data;
+
+    public JCFReadStatusRepository() {
+        this.data = new HashMap<>();
+    }
+
+    @Override
+    public void save(ReadStatus readStatus) {
+        data.put(readStatus.getId(), readStatus);
+    }
+
+    @Override
+    public Optional<ReadStatus> findById(UUID readStatusId) {
+        return Optional.ofNullable(data.get(readStatusId));
+    }
+
+    @Override
+    public Optional<ReadStatus> findByUserIdAndChannelId(UUID userId, UUID channelId) {
+        return data.values().stream()
+                .filter(readStatus -> readStatus.getUserId().equals(userId) && readStatus.getChannelId().equals(channelId))
+                .findFirst();
+    }
+
+    @Override
+    public List<ReadStatus> findAllByUserId(UUID userId) {
+        return data.values().stream()
+                .filter(readStatus -> readStatus.getUserId().equals(userId))
+                .toList();
+    }
+
+    @Override
+    public List<ReadStatus> findAllByChannelId(UUID channelId) {
+        return data.values().stream()
+                .filter(readStatus -> readStatus.getChannelId().equals(channelId))
+                .toList();
+    }
+
+    @Override
+    public void delete(UUID readStatusId) {
+        data.remove(readStatusId);
+    }
+
+    @Override
+    public boolean existReadStatus(UUID userId, UUID channelId) {
+        return data.values().stream()
+                .anyMatch(readStatus -> readStatus.getUserId().equals(userId) && readStatus.getChannelId().equals(channelId));
+    }
+}
--- src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFUserRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFUserRepository.java
@@ -2,9 +2,11 @@ package com.sprint.mission.discodeit.repository.jcf;
 
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.repository.UserRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class JCFUserRepository implements UserRepository {
     private final Map<UUID, User> data;
 
@@ -25,7 +27,7 @@ public class JCFUserRepository implements UserRepository {
     @Override
     public Optional<User> findByUserNameAndPassword(String userName, String password) {
         return this.data.values().stream()
-                .filter(u -> u.getEmail().equals(userName) && u.getPassword().equals(password))
+                .filter(u -> u.getUserName().equals(userName) && u.getPassword().equals(password))
                 .findFirst();
     }
 
