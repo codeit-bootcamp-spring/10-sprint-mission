1a67cdfd02cebf22caf6d5f6cfb7aad2b3667811
JungH200000 <jhpaxk0604@gmail.com>
2026-01-30 19:04:05 +0900

    refactor: 파일 경로를 `application.yml` 설정 정보로 이동

--- src/main/java/com/sprint/mission/discodeit/repository/file/FileObjectStore.java
+++ src/main/java/com/sprint/mission/discodeit/repository/file/FileObjectStore.java
@@ -12,8 +12,9 @@ import java.util.UUID;
 
 @Getter
 public class FileObjectStore implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
-    private static final Path STORAGE_PATH = Path.of("./FileIOTestStorage/objectStorage.ser");
+    private transient Path storagePath;
 
     private final Map<UUID, User> usersData = new HashMap<>();
     private final Map<UUID, Channel> channelsData = new HashMap<>();
@@ -22,28 +23,33 @@ public class FileObjectStore implements Serializable {
     private final Map<UUID, BinaryContent> binaryContentsData = new HashMap<>();
     private final Map<UUID, ReadStatus> readStatusesData = new HashMap<>();
 
+    public FileObjectStore(Path storagePath) {
+        this.storagePath = storagePath;
+    }
+
     public void saveData() {
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(STORAGE_PATH.toFile()))) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(storagePath.toFile()))) {
             oos.writeObject(this);
         } catch (IOException | IllegalStateException e) {
             System.out.println(e);
         }
     }
-    public static FileObjectStore loadData() {
+    public static FileObjectStore loadData(Path storagePath) {
         // 파일 존재 X
-        if (!Files.exists(STORAGE_PATH)) {
-            return new FileObjectStore();
+        if (!Files.exists(storagePath)) {
+            return new FileObjectStore(storagePath);
         }
 
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(STORAGE_PATH.toFile()))) {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(storagePath.toFile()))) {
             Object fileData = ois.readObject();
             if (fileData instanceof FileObjectStore) {
                 FileObjectStore fileObjectStore = (FileObjectStore) fileData;
+                fileObjectStore.storagePath = storagePath;
                 return fileObjectStore;
             }
         } catch (IOException | ClassNotFoundException e) {
             System.out.println(e);
         }
-        return new FileObjectStore(); // 파일이 존재하지만 읽기 실패 시
+        return new FileObjectStore(storagePath); // 파일이 존재하지만 읽기 실패 시
     }
 }
