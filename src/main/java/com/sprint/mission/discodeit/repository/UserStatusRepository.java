d6c0b0e452e434817495ec0ec4af9aea33b41fe7
JunSuHwang <wnstn9904@naver.com>
2026-01-30 09:30:24 +0900

    feat: UserStatusRepository에 userId로 삭제하는 메서드 추가

--- src/main/java/com/sprint/mission/discodeit/repository/UserStatusRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/UserStatusRepository.java
@@ -12,4 +12,5 @@ public interface UserStatusRepository {
     List<UserStatus> findAll();
     void save(UserStatus userStatus);
     void deleteById(UUID id);
+    void deleteByUserId(UUID userId);
 }
