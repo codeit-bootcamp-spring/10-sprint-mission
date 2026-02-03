b1c5ddd7c89ea954b6acf9162ea9a42f43432d9b
jiho0420 <jiho0420@gachon.ac.kr>
2026-02-03 17:32:26 +0900

    test코드 구현

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -1,20 +1,13 @@
 package com.sprint.mission.discodeit;
 
-import com.sprint.mission.discodeit.dto.*;
-import com.sprint.mission.discodeit.service.ChannelService;
-import com.sprint.mission.discodeit.service.MessageService;
-import com.sprint.mission.discodeit.service.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
-import java.util.List;
 
 @SpringBootApplication
 public class DiscodeitApplication {
 
     public static void main(String[] args) {
-        // 1. 컨텍스트 로드 (설정에 따라 File 구현체가 주입됨)
-        ConfigurableApplicationContext context = SpringApplication.run(DiscodeitApplication.class, args);
+        SpringApplication.run(DiscodeitApplication.class, args);
     }
 }
--- src/main/java/com/sprint/mission/discodeit/dto/UserStatusDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/UserStatusDto.java
@@ -2,10 +2,11 @@ package com.sprint.mission.discodeit.dto;
 
 import com.sprint.mission.discodeit.entity.UserStatus;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.time.Instant;
 import java.util.UUID;
-
+@Getter
 @AllArgsConstructor
 public class UserStatusDto {
     private UUID id;
