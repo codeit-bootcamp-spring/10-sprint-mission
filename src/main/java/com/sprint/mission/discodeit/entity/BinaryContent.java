65067f3a0fc41724fb321a124b73e5e9bb828eec
parkngg <millco0803com@gmail.com>
2026-02-03 17:14:15 +0900

    refactor: BinaryContent 엔티티 updatedAt 필드 제거

--- src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
+++ src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
@@ -4,18 +4,24 @@ import lombok.Getter;
 import lombok.ToString;
 
 import java.io.Serial;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.UUID;
 
 @Getter
 @ToString
-public class BinaryContent extends BaseEntity{
+public class BinaryContent implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
+    private final UUID id;
+    private final Instant createdAt;
     private final String fileName;
     private final byte[] data;
 
     public BinaryContent(String fileName, byte[] data) {
-        super();
+        this.id = UUID.randomUUID();
+        this.createdAt = Instant.now();
         this.fileName = fileName;
         this.data = data;
     }
