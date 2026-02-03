0ec65a9d1fb2c10f53b956e003f230efb830da3a
JunSuHwang <wnstn9904@naver.com>
2026-02-03 11:24:21 +0900

    feat: ReadStatusService 구현 및 고도화 적용

---
+++ src/main/java/com/sprint/mission/discodeit/service/ReadStatusService.java
@@ -0,0 +1,65 @@
+package com.sprint.mission.discodeit.service;
+
+import com.sprint.mission.discodeit.dto.readstatus.ReadStatusCreateInfo;
+import com.sprint.mission.discodeit.dto.readstatus.ReadStatusInfo;
+import com.sprint.mission.discodeit.dto.readstatus.ReadStatusUpdateInfo;
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.entity.ReadStatus;
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.mapper.ReadStatusMapper;
+import com.sprint.mission.discodeit.repository.ChannelRepository;
+import com.sprint.mission.discodeit.repository.ReadStatusRepository;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+public class ReadStatusService {
+    private final ReadStatusRepository readStatusRepository;
+    private final UserRepository userRepository;
+    private final ChannelRepository channelRepository;
+
+    public ReadStatusInfo createReadStatus(ReadStatusCreateInfo statusInfo) {
+        User user = userRepository.findById(statusInfo.userId())
+                .orElseThrow(() -> new NoSuchElementException("해당 사용자가 존재하지 않습니다."));
+        Channel channel = channelRepository.findById(statusInfo.channelId())
+                .orElseThrow(() -> new NoSuchElementException("해당 채널이 존재지 않습니다."));
+
+        if(readStatusRepository.findByUserIdAndChannelId(user.getId(), channel.getId())
+                .isPresent()) throw new IllegalStateException("읽기 상태가 이미 존재합니다.");
+
+        ReadStatus readStatus = new ReadStatus(channel.getId(), user.getId());
+        readStatusRepository.save(readStatus);
+        return ReadStatusMapper.toReadStatusInfo(readStatus);
+    }
+
+    public ReadStatusInfo find(UUID statusId) {
+        ReadStatus readStatus = readStatusRepository.findById(statusId)
+                .orElseThrow(() -> new NoSuchElementException("해당 읽기 상태가 존재하지 않습니다."));
+        return ReadStatusMapper.toReadStatusInfo(readStatus);
+    }
+
+    public List<ReadStatusInfo> findAllByUserId(UUID userId) {
+        return readStatusRepository.findAllByUserId(userId)
+                .stream()
+                .map(ReadStatusMapper::toReadStatusInfo)
+                .toList();
+    }
+
+    public ReadStatusInfo updateReadStatus(ReadStatusUpdateInfo updateInfo) {
+        ReadStatus readStatus = readStatusRepository.findById(updateInfo.statusId())
+                .orElseThrow(() -> new NoSuchElementException("해당 읽기 상태가 존재하지 않습니다."));
+        readStatus.updateLastReadAt();
+        readStatusRepository.save(readStatus);
+        return ReadStatusMapper.toReadStatusInfo(readStatus);
+    }
+
+    public void deleteReadStatus(UUID statusId) {
+        readStatusRepository.deleteById(statusId);
+    }
+}
