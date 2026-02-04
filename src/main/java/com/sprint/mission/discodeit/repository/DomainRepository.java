cfed46381ac5e0ff2bdf119fbe2f8990c293cbc8
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2026-02-04 14:05:16 +0900

    feat(DomainService): remove findAll, code reformatting
    - DomainService: remove findAll
    - DomainRepository: code reformatting

--- src/main/java/com/sprint/mission/discodeit/repository/DomainRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/DomainRepository.java
@@ -6,8 +6,12 @@ import java.util.UUID;
 
 public interface DomainRepository<T> {
     T save(T entity);
+
     Optional<T> findById(UUID id);
+
     List<T> findAll();
+
     boolean existsById(UUID id);
+
     void deleteById(UUID id);
 }
--- src/main/java/com/sprint/mission/discodeit/service/DomainService.java
+++ src/main/java/com/sprint/mission/discodeit/service/DomainService.java
@@ -1,14 +1,11 @@
 package com.sprint.mission.discodeit.service;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface DomainService<F, C, U> {
     F create(C model);
 
     F find(UUID id);
-
-    List<F> findAll();
 
     F update(U model);
 
