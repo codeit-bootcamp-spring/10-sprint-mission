71a169efc2ae295f11dc0b1c6ff9e10b0e609623
backtobasic27 <rlagptjd27@gmail.com>
2026-02-04 15:07:09 +0900

    UserStatusService 구현 - UserStatusService 인터페이스 구현

--- src/main/java/com/sprint/mission/discodeit/service/status/UserStatusService.java
+++ src/main/java/com/sprint/mission/discodeit/service/status/UserStatusService.java
@@ -1,4 +1,24 @@
 package com.sprint.mission.discodeit.service.status;
 
+import com.sprint.mission.discodeit.dto.userstatus.CreateUserStatusRequest;
+import com.sprint.mission.discodeit.dto.userstatus.UpdateUserStatusRequest;
+import com.sprint.mission.discodeit.dto.userstatus.UserStatusResponse;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface UserStatusService {
+
+    UserStatusResponse create(CreateUserStatusRequest request);
+
+    UserStatusResponse find(UUID id);
+
+    List<UserStatusResponse> findAll();
+
+    UserStatusResponse update(UpdateUserStatusRequest request);
+
+    UserStatusResponse updateByUserId(UUID userId, UpdateUserStatusRequest request);
+
+    void delete(UUID id);
+    
 }
