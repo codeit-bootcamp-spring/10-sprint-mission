c9eb4977e40ab9e84b3064306932b7f06ffe23d9
초시맥북 <fyg7575@gmail.com>
2026-02-06 20:38:26 +0900

    MessageController: 메시지 발행 메서드 작성

--- src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
@@ -1,22 +1,26 @@
 package com.sprint.mission.discodeit.controller;
 
 import com.sprint.mission.discodeit.dto.message.MessageCreateRequestDto;
+import com.sprint.mission.discodeit.dto.message.MessageResponseDto;
 import com.sprint.mission.discodeit.service.MessageService;
 import lombok.AllArgsConstructor;
-import org.apache.coyote.Request;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("/messages")
 public class MessageController {
     private final MessageService messageService;
-/*
-    @RequestMapping(method = RequestMethod.POST)
-    public void postMessage(
-            @RequestPart("dto") MessageCreateRequestDto
-            ){
 
+    @RequestMapping(method = RequestMethod.POST)
+    public MessageResponseDto postMessage(
+            @RequestPart("dto") MessageCreateRequestDto requestDto,
+            @RequestPart(value = "attachments", required = false) List<MultipartFile> attachments
+            ){
+        return messageService.create(requestDto, attachments);
     }
-    */
+
 }
