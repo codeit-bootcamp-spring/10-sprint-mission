6ab8066fa5a1715377bc0f392770596ff8649959
Minbro-Kim <sojangjangi@gmail.com>
2026-02-06 19:20:34 +0900

    refactor: MessageCreateDto.java 에서 사용자와 채널 아이디 삭제

--- src/main/java/com/sprint/mission/discodeit/dto/message/MessageCreateDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/message/MessageCreateDto.java
@@ -1,14 +1,13 @@
 package com.sprint.mission.discodeit.dto.message;
 
 import com.sprint.mission.discodeit.dto.binarycontent.BinaryContentCreateDto;
+import jakarta.validation.constraints.NotEmpty;
 
 import java.util.List;
 import java.util.UUID;
 
 public record MessageCreateDto(
         String content,
-        UUID channelId,
-        UUID authorId,
         List<BinaryContentCreateDto> attachments
 ) {
 }
