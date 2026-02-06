6f1495f96a85d1f6f708a48a78d5dabafb6218bc
초시맥북 <fyg7575@gmail.com>
2026-02-06 18:16:13 +0900

    LoginResponseDto: 로그인 시 전달할 사용자 정보를 dto로 변경

--- src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
@@ -1,4 +1,13 @@
 package com.sprint.mission.discodeit.controller;
 
+import com.sprint.mission.discodeit.service.MessageService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@AllArgsConstructor
+@Controller
+@RequestMapping("/messages")
 public class MessageController {
+    private final MessageService messageService;
 }
