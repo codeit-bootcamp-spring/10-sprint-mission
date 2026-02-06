658366249f122c75d49d247d98f5bb546ed6d4d4
Minbro-Kim <sojangjangi@gmail.com>
2026-02-06 18:16:58 +0900

    feat: PrivateChannelCreateDto.java와 & PublicChannelCreateDto.java 유효성 검증 추가

--- src/main/java/com/sprint/mission/discodeit/dto/channel/PrivateChannelCreateDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/channel/PrivateChannelCreateDto.java
@@ -1,11 +1,13 @@
 package com.sprint.mission.discodeit.dto.channel;
 
 import com.sprint.mission.discodeit.entity.User;
+import jakarta.validation.constraints.NotEmpty;
 
 import java.util.List;
 import java.util.UUID;
 
 public record PrivateChannelCreateDto(
+        @NotEmpty
         List<UUID> memberIds
 ) {
 }
