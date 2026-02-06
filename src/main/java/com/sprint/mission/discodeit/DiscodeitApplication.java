f1e612d7b2d7b24f3baed477b3d97b33d2db9fd3
초시맥북 <fyg7575@gmail.com>
2026-02-06 14:45:33 +0900

    DisCodeitApplication: 테스트 코드 삭제

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -25,54 +25,8 @@ import java.util.UUID;
 
 @SpringBootApplication
 public class DiscodeitApplication {
-	static UserResponseDto setupUser(UserService userService) {
-		return userService.create(
-				new UserCreateRequestDto(
-						"woody",
-						"woody@codeit.com",
-						"woody1234",
-						null));
-	}
-
-	static ChannelResponseDto setupPublicChannel(ChannelService channelService) {
-		return channelService.createPublicChannel(
-				new PublicChannelCreateRequestDto("공지", "공지 채널입니다.")
-		);
-	}
-
-	static ChannelResponseDto setupPrivateChannel(ChannelService channelService, List<UUID> userIds) {
-		return channelService.createPrivateChannel(
-				new PrivateChannelCreateRequestDto(userIds)
-		);
-	}
-
-	static void messageCreateTest(MessageService messageService, ChannelResponseDto channel, UserResponseDto author) {
-		MessageResponseDto message = messageService.create(
-				new MessageCreateRequestDto("안녕하세요.", channel.id(), author.id(), null)
-		);
-		System.out.println("메시지 생성: " + message.id() + " " + message.content() + "in " + channel.name());
-	}
-
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context =
-				SpringApplication.run(DiscodeitApplication.class, args);
-
-		UserService userService = context.getBean(UserService.class);
-		ChannelService channelService = context.getBean(ChannelService.class);
-		MessageService messageService = context.getBean(MessageService.class);
-		AuthService authService = context.getBean(AuthService.class);
-
-		// 셋업
-		UserResponseDto user1 = setupUser(userService);
-		ChannelResponseDto channel1 = setupPublicChannel(channelService);
-
-		List<UUID> userIds = new ArrayList<>();
-		userIds.add(user1.id());
-		ChannelResponseDto channel2 = setupPrivateChannel(channelService, userIds);
-		// 테스트
-		messageCreateTest(messageService, channel1, user1);
-		messageCreateTest(messageService, channel2, user1);
-
+		SpringApplication.run(DiscodeitApplication.class, args);
 	}
 
 }
