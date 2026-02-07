cbb6240d3ae3148355bd6a88f8eb0d21c046943c
초시맥북 <fyg7575@gmail.com>
2026-02-08 01:52:49 +0900

    MessageResponseDto: 메시지가 생성된 시각도 함께 응답하도록 수정함.

--- src/main/java/com/sprint/mission/discodeit/dto/message/MessageResponseDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/message/MessageResponseDto.java
@@ -1,9 +1,11 @@
 package com.sprint.mission.discodeit.dto.message;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 
 public record MessageResponseDto(
+        Instant createdAt,
         UUID id,
         String content,
         UUID channelId,
