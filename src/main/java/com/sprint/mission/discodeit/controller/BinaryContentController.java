d82b042acb5903c82b2f4b7e7e1c52c89a575015
초시맥북 <fyg7575@gmail.com>
2026-02-07 22:58:49 +0900

    BinaryContentController: 파일 단일 조회, 메시지 내 모든 파일 조회 메서드 구현함.

--- src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
@@ -1,4 +1,41 @@
 package com.sprint.mission.discodeit.controller;
 
+import com.sprint.mission.discodeit.dto.binarycontent.BinaryContentResponseDto;
+import com.sprint.mission.discodeit.service.BinaryContentService;
+import com.sprint.mission.discodeit.service.ChannelService;
+import com.sprint.mission.discodeit.service.MessageService;
+import com.sprint.mission.discodeit.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@AllArgsConstructor
 public class BinaryContentController {
+    private final BinaryContentService binaryContentService;
+    private final ChannelService channelService;
+    private final MessageService messageService;
+
+    //이미지 단순 조회(프로필, 메시지 내 파일 단일 모두 가능)
+    @RequestMapping(value = "/files/{fileId}", method = RequestMethod.GET)
+    public BinaryContentResponseDto getSimpleFiles(
+            @PathVariable UUID fileId
+    ){
+        return binaryContentService.find(fileId);
+    }
+
+    //메시지 내부 이미지 전원 조회
+    @RequestMapping(value = "/channels/{channelId}/messages/{messageId}/files")
+    public List<BinaryContentResponseDto> getMessagesFiles(
+            @PathVariable UUID messageId
+    ){
+        return messageService.find(messageId).attachments().stream()
+                .map(binaryContentService::find)
+                .toList();
+    }
 }
