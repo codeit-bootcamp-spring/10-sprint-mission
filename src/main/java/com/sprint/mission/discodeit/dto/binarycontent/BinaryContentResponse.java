28f3e8d9b71ddebac592a9ca86dc40569eb3f4ff
backtobasic27 <rlagptjd27@gmail.com>
2026-02-07 09:47:38 +0900

    BinaryContentService 고도화 - from( ) 메서드 구현 완료

--- src/main/java/com/sprint/mission/discodeit/dto/binarycontent/BinaryContentResponse.java
+++ src/main/java/com/sprint/mission/discodeit/dto/binarycontent/BinaryContentResponse.java
@@ -1,9 +1,17 @@
 package com.sprint.mission.discodeit.dto.binarycontent;
 
+import com.sprint.mission.discodeit.entity.status.BinaryContent;
+import lombok.*;
+
+import javax.swing.text.AbstractDocument;
 import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class BinaryContentResponse {
     private UUID id;
     private String fileName;
@@ -15,4 +23,16 @@ public class BinaryContentResponse {
 
     //data는 보안/크기 이유로 응답에서 제외
     // 다운로드 API 구현할 예정
+    
+    public static BinaryContentResponse from(BinaryContent content) {
+        return BinaryContentResponse.builder()
+                .id(content.getId())
+                .fileName(content.getFileName())
+                .contentType(content.getContentType())
+                .size(content.getSize())
+                .attachmentId(content.getAttachmentId())
+                .createdAt(content.getCreatedAt())
+                .updatedAt(content.getUpdatedAt())
+                .build();
+    }
 }
