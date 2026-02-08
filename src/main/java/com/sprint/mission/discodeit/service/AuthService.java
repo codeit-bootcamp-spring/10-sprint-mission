532564361aad1ce263826c0ca23437742a465453
JungH200000 <jhpaxk0604@gmail.com>
2026-02-08 14:42:49 +0900

    refactor: `request`와 `response` 패키지 분류로 `import` 경로 변경

--- src/main/java/com/sprint/mission/discodeit/service/AuthService.java
+++ src/main/java/com/sprint/mission/discodeit/service/AuthService.java
@@ -1,7 +1,7 @@
 package com.sprint.mission.discodeit.service;
 
 import com.sprint.mission.discodeit.dto.auth.LoginRequest;
-import com.sprint.mission.discodeit.dto.user.UserWithOnlineResponse;
+import com.sprint.mission.discodeit.dto.user.response.UserWithOnlineResponse;
 
 public interface AuthService {
 
