f6a1bfaafaeb8ed0c638653c474571e6c477b508
JungH200000 <jhpaxk0604@gmail.com>
2026-02-08 20:50:46 +0900

    feat: `MessageController` 구현

---
+++ src/main/java/com/sprint/mission/discodeit/dto/message/request/MessageUpdateRequest.java
@@ -0,0 +1,14 @@
+package com.sprint.mission.discodeit.dto.message.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.UUID;
+
+public record MessageUpdateRequest(
+        @NotNull(message = "ID가 null입니다.")
+        UUID requestUserId,
+        @NotBlank(message = "content가 입력되지 않았습니다.")
+        String content
+) {
+}
