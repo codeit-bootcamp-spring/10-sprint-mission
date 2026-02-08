39cdd5126db17664d3aa51a74d7a2feb38ddba9f
parkngg <millco0803com@gmail.com>
2026-02-09 04:10:03 +0900

    feat: 예외 처리기(GlobalExceptionHandler) 추가

---
+++ src/main/java/com/sprint/mission/discodeit/controller/advice/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.sprint.mission.discodeit.controller.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNotFound(NoSuchElementException e){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleBadRequest(IllegalArgumentException e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+}
