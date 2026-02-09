fda8b724e8a11ae7f34439bc85d2c14950e63fa2
LeeDyol <dlekthf819203@naver.com>
2026-02-09 16:42:26 +0900

    feat: 로그인 기능을 위한 AuthController 구현

--- src/main/java/com/sprint/mission/discodeit/service/AuthService.java
+++ src/main/java/com/sprint/mission/discodeit/service/AuthService.java
@@ -1,9 +1,9 @@
 package com.sprint.mission.discodeit.service;
 
 import com.sprint.mission.discodeit.dto.request.auth.AuthLoginRequestDTO;
-import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.dto.response.UserResponseDTO;
 
 public interface AuthService {
     // 로그인
-    User login(AuthLoginRequestDTO authLoginRequestDTO);
+    UserResponseDTO login(AuthLoginRequestDTO authLoginRequestDTO);
 }
