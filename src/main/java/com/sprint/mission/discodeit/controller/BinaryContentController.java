1b6711299cbc91d2d9a8435a00537a79a5189c4f
shong9124 <shifty393@gmail.com>
2026-02-09 15:04:13 +0900

    [feat] BinaryContentController 구현

---
+++ src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
@@ -0,0 +1,37 @@
+package com.sprint.mission.discodeit.controller;
+
+import com.sprint.mission.discodeit.dto.binarycontent.BinaryContentResponseDTO;
+import com.sprint.mission.discodeit.service.BinaryContentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/v1/binary-contents")
+@RequiredArgsConstructor
+public class BinaryContentController {
+
+    private final BinaryContentService binaryContentService;
+
+    @RequestMapping(value = "/find-all", method = RequestMethod.GET)
+    public ResponseEntity findAll() {
+        List<BinaryContentResponseDTO> binaryContents = binaryContentService.findAll();
+
+        return ResponseEntity.ok(binaryContents);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity findById(
+            @RequestParam UUID binaryContentId
+    ) {
+        BinaryContentResponseDTO binaryContent = binaryContentService.findById(binaryContentId);
+
+        return ResponseEntity.ok(binaryContent);
+    }
+}
