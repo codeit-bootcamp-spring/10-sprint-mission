8e290776faaf1e33f952ea15058b8e2d7b9aeef4
raonPsm <s102075@naver.com>
2026-02-09 17:18:39 +0900

    ChannelController 3차 구현

--- src/main/java/com/sprint/mission/discodeit/controller/ChannelController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/ChannelController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -49,10 +50,15 @@ public class ChannelController {
 
     // 채널을 삭제할 수 있다.
     @RequestMapping(value = "/{channelId}", method = RequestMethod.DELETE)
-    public ResponseEntity<ChannelResponse> delete(@PathVariable UUID channelId) {
+    public ResponseEntity<Void> delete(@PathVariable UUID channelId) {
         channelService.delete(channelId);
-        return ResponseEntity.notFound().build();
+        return ResponseEntity.noContent().build();
     }
 
     // 특정 사용자가 볼 수 있는 모든 채널 목록을 조회할 수 있다.
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<ChannelResponse>> findAllByUserId(@RequestParam UUID userId) {
+        List<ChannelResponse> responses = channelService.findAllByUserId(userId);
+        return ResponseEntity.status(HttpStatus.OK).body(responses);
+    }
 }
