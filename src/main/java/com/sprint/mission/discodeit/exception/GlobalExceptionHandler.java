6190202e46af87339513d834cab5362b34bfc5a7
JunSuHwang <wnstn9904@naver.com>
2026-02-09 17:22:09 +0900

    fix: UserStatus 예외 누락 추가

--- src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package com.sprint.mission.discodeit.exception;
 import com.sprint.mission.discodeit.user.exception.EmailDuplicationException;
 import com.sprint.mission.discodeit.user.exception.UserDuplicationException;
 import com.sprint.mission.discodeit.user.exception.UserNotFoundException;
+import com.sprint.mission.discodeit.userstatus.exception.UserStatusDuplicationException;
+import com.sprint.mission.discodeit.userstatus.exception.UserStatusNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,21 +14,33 @@ import org.springframework.web.method.annotation.MethodArgumentTypeMismatchExcep
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handle(UserNotFoundException e) {
-        ErrorResponse response = new ErrorResponse("USER_NOT_FOUND", e.getMessage());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
-    }
-
     @ExceptionHandler(EmailDuplicationException.class)
     public ResponseEntity<ErrorResponse> handle(EmailDuplicationException e) {
         ErrorResponse response = new ErrorResponse("EMAIL_DUPLICATION_ERROR", e.getMessage());
         return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
     }
 
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handle(UserNotFoundException e) {
+        ErrorResponse response = new ErrorResponse("USER_NOT_FOUND", e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
     @ExceptionHandler(UserDuplicationException.class)
     public ResponseEntity<ErrorResponse> handle(UserDuplicationException e) {
         ErrorResponse response = new ErrorResponse("USER_DUPLICATION_ERROR", e.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
+    }
+
+    @ExceptionHandler(UserStatusNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handle(UserStatusNotFoundException e) {
+        ErrorResponse response = new ErrorResponse("USER_STATUS_NOT_FOUND", e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+    @ExceptionHandler(UserStatusDuplicationException.class)
+    public ResponseEntity<ErrorResponse> handle(UserStatusDuplicationException e) {
+        ErrorResponse response = new ErrorResponse("USER_STATUS_DUPLICATION_ERROR", e.getMessage());
         return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
     }
 
