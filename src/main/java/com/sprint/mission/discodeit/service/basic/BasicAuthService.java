9b68508d047e7479431341fe3482fb09721227c5
LeeDyol <dlekthf819203@naver.com>
2026-02-09 16:42:26 +0900

    feat: 로그인 기능을 위한 AuthController 구현

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
@@ -1,8 +1,11 @@
 package com.sprint.mission.discodeit.service.basic;
 
 import com.sprint.mission.discodeit.dto.request.auth.AuthLoginRequestDTO;
+import com.sprint.mission.discodeit.dto.response.UserResponseDTO;
 import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.entity.UserStatus;
 import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.repository.UserStatusRepository;
 import com.sprint.mission.discodeit.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,17 +14,27 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class BasicAuthService implements AuthService {
     private final UserRepository userRepository;
+    private final UserStatusRepository userStatusRepository;
 
     // 로그인
-    public User login(AuthLoginRequestDTO authLoginRequestDTO) {
+    public UserResponseDTO login(AuthLoginRequestDTO authLoginRequestDTO) {
         User targetUser = userRepository.findById(authLoginRequestDTO.getUserId())
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 존재하지 않습니다."));
+        UserStatus targetUserStatus = userStatusRepository.findByUserId(targetUser.getId());
 
         if (!targetUser.getPassword().equals(authLoginRequestDTO.getPassword()) ||
                 !targetUser.getNickname().equals(authLoginRequestDTO.getNickname())){
             throw new IllegalArgumentException(("[로그인 실패] 잘못된 닉네임 혹은 비밀번호를 입력하셨습니다."));
         }
 
-        return targetUser;
+        return UserResponseDTO.builder()
+                .id(targetUser.getId())
+                .nickname(targetUser.getNickname())
+                .email(targetUser.getEmail())
+                .status(targetUserStatus.getStatus())
+                .profileId(targetUser.getProfileId())
+                .createdAt(targetUser.getCreatedAt())
+                .updatedAt(targetUser.getUpdatedAt())
+                .build();
     }
 }
