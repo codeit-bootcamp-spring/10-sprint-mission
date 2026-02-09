3bb65ece6cdfd34a0fa654cd4a35990d44410209
LeeDyol <dlekthf819203@naver.com>
2026-02-09 17:27:11 +0900

    feat: 매사자 CRUD 및 특정 채널에서 발행된 메시지 목록 조회 API 구현

---
+++ src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
@@ -0,0 +1,54 @@
+package com.sprint.mission.discodeit.controller;
+
+import com.sprint.mission.discodeit.dto.request.message.MessageCreateRequestDTO;
+import com.sprint.mission.discodeit.dto.request.message.MessageUpdateRequestDTO;
+import com.sprint.mission.discodeit.dto.response.MessageResponseDTO;
+import com.sprint.mission.discodeit.service.MessageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/messages")
+@RequiredArgsConstructor
+public class MessageController {
+    private final MessageService messageService;
+
+    // 메시지 생성
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<MessageResponseDTO> create(@RequestBody MessageCreateRequestDTO messageCreateRequestDTO) {
+        MessageResponseDTO newMessage = messageService.create(messageCreateRequestDTO);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(newMessage);
+    }
+
+    // 특정 채널에서 발행된 메시지 목록 조회
+    @RequestMapping(value = "/{channelId}", method = RequestMethod.GET)
+    public ResponseEntity<List<MessageResponseDTO>> findAllByChannelId (@PathVariable UUID channelId) {
+        List<MessageResponseDTO> messages = messageService.findAllByChannelId(channelId);
+
+        return ResponseEntity.ok(messages);
+    }
+
+    // 메시지 수정
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<MessageResponseDTO> update(@PathVariable UUID id,
+                                                     @RequestBody MessageUpdateRequestDTO messageUpdateRequestDTO) {
+        messageUpdateRequestDTO.setId(id);
+        MessageResponseDTO updateMessage = messageService.update(messageUpdateRequestDTO);
+
+        return ResponseEntity.ok(updateMessage);
+    }
+
+    // 메시지 삭제
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<MessageResponseDTO> delete(@PathVariable UUID id) {
+        messageService.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
+}
