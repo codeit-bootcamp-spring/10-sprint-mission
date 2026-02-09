99eeaa67c9fc50ef2aedc48a4a411ba1a3a507f4
LeeDyol <dlekthf819203@naver.com>
2026-02-09 17:27:11 +0900

    feat: 매사자 CRUD 및 특정 채널에서 발행된 메시지 목록 조회 API 구현

--- src/main/java/com/sprint/mission/discodeit/dto/request/message/MessageUpdateRequestDTO.java
+++ src/main/java/com/sprint/mission/discodeit/dto/request/message/MessageUpdateRequestDTO.java
@@ -2,10 +2,7 @@ package com.sprint.mission.discodeit.dto.request.message;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.UUID;
 
@@ -14,6 +11,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MessageUpdateRequestDTO {
+    @Setter
     @NotNull
     private UUID id;
 
