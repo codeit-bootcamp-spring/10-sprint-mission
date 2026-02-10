5d065db8ddb205a384a952368142aa9be56c146e
LeeDyol <dlekthf819203@naver.com>
2026-02-10 13:54:23 +0900

    Revert "feat: 사용자 CRUD 및 상태 업데이트를 위한 UserController 구현"
    This reverts commit 7e272140e48ad9d639c17a79aec3eafd7f15bbb5.

--- src/main/java/com/sprint/mission/discodeit/dto/request/user/UserUpdateRequestDTO.java
+++ src/main/java/com/sprint/mission/discodeit/dto/request/user/UserUpdateRequestDTO.java
@@ -5,16 +5,19 @@ import com.sprint.mission.discodeit.dto.request.userStatus.UserStatusUpdateReque
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Getter
+
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserUpdateRequestDTO {
-    @Setter
     @NotNull
     private UUID id;
 
