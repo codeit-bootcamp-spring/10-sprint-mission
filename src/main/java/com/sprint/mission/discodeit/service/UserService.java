cf0b66c9fe036a4a1e8e74a8d2cf9bea463d30c7
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2026-02-10 11:28:04 +0900

    feat(controller, service, util): init UserController, add feature to UserService, init RandomUUID, rename UserServiceDTO, add config to application.yml
    - UserService: add updateActiveAt
    - BasicUserService: implement updateActiveAt
    - User: change constructor parameter; uuid injected
    - UserServiceDTO: rename
    - BasicDomainService: add deleteIfExist

--- src/main/java/com/sprint/mission/discodeit/service/UserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/UserService.java
@@ -1,11 +1,18 @@
 package com.sprint.mission.discodeit.service;
 
-import com.sprint.mission.discodeit.dto.UserServiceDTO.*;
+import com.sprint.mission.discodeit.dto.AuthServiceDTO.LoginRequest;
+import com.sprint.mission.discodeit.dto.UserServiceDTO.UserCreateRequest;
+import com.sprint.mission.discodeit.dto.UserServiceDTO.UserResponse;
+import com.sprint.mission.discodeit.dto.UserServiceDTO.UserUpdateRequest;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
-public interface UserService extends DomainService<UserResponse, UserCreation, UserInfoUpdate> {
-    UserResponse find(UsernamePassword model) throws IOException;
+public interface UserService extends DomainService<UserResponse, UserCreateRequest, UserUpdateRequest> {
+    UserResponse find(LoginRequest dto) throws IOException;
+
     List<UserResponse> findAll() throws IOException, ClassNotFoundException;
+
+    UserResponse updateActiveAt(UUID id) throws IOException, ClassNotFoundException;
 }
