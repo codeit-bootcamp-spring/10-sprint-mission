7f18683faadf60085372b5efac809883b449bfc0
JunSuHwang <wnstn9904@naver.com>
2026-02-11 09:16:12 +0900

    feat: 바이너리 파일 단건 조회 추가

--- src/main/java/com/sprint/mission/discodeit/binarycontent/controller/BinaryContentController.java
+++ src/main/java/com/sprint/mission/discodeit/binarycontent/controller/BinaryContentController.java
@@ -5,18 +5,21 @@ import com.sprint.mission.discodeit.binarycontent.dto.BinaryContentsRequest;
 import com.sprint.mission.discodeit.binarycontent.service.BinaryContentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/binarycontents")
 public class BinaryContentController {
     private final BinaryContentService binaryContentService;
+
+    @RequestMapping(value = "/{contentId}", method = RequestMethod.GET)
+    public ResponseEntity<BinaryContentInfo> getBinaryContent(@PathVariable UUID contentId) {
+        return ResponseEntity.ok(binaryContentService.findBinaryContent(contentId));
+    }
 
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<BinaryContentInfo>> getBinaryContents(
