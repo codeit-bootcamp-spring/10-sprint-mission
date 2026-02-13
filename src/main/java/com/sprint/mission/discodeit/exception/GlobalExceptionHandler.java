79f7296fb59405f5275d4841749a91fd6a7c0580
Juuu00 <sungjuhyun1001@gmail.com>
2026-02-13 15:42:25 +0900

    검증 실패 시 설정한 메시지를 반환하도록 수정

--- src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.sprint.mission.discodeit.exception;
 
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -17,7 +19,15 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorDto> handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
         ErrorCode code = ErrorCode.VALIDATION_ERROR;
-        return ResponseEntity.status(code.getHttpStatus()).body(ErrorDto.of(code, code.getDefaultMessage()));
+
+        String message = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .findFirst()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .orElse(code.getDefaultMessage());
+
+        return ResponseEntity.status(code.getHttpStatus()).body(ErrorDto.of(code, message));
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
