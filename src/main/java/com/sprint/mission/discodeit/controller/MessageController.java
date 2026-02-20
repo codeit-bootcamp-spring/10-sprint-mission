0dff214a5cbb0daa4f64c9100b07d76d8b5125b5
kwakinsung <kis2690@gmail.com>
2026-02-20 15:33:33 +0900

    Message REST API 설계

--- src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
@@ -1,47 +1,76 @@
 package com.sprint.mission.discodeit.controller;
 
+import com.sprint.mission.discodeit.dto.request.BinaryContentCreateRequest;
 import com.sprint.mission.discodeit.dto.request.MessageCreateRequest;
 import com.sprint.mission.discodeit.dto.request.MessageUpdateRequest;
 import com.sprint.mission.discodeit.entity.Message;
 import com.sprint.mission.discodeit.service.MessageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-@Controller
-@RequestMapping("/messages")
+@RestController
+@RequestMapping("/api/messages")
 @RequiredArgsConstructor
 public class MessageController {
 
     private final MessageService messageService;
 
-    @RequestMapping(method = RequestMethod.POST)
-    @ResponseBody
-    public Message sendMessage(@RequestBody MessageCreateRequest messageCreateRequest) {
-        return messageService.create(messageCreateRequest);
+    @PostMapping
+    public ResponseEntity<Message> sendMessage(@RequestPart MessageCreateRequest messageCreateRequest,
+                                              @RequestPart(required = false) List<MultipartFile> attachments) throws IOException {
+        List<BinaryContentCreateRequest> binaryContentCreateRequests = new ArrayList<>();
+
+        if (attachments != null) {
+            for (MultipartFile file : attachments) {
+                BinaryContentCreateRequest request = new BinaryContentCreateRequest(
+                        file.getOriginalFilename(),
+                        file.getContentType(),
+                        file.getBytes()
+                );
+                binaryContentCreateRequests.add(request);
+            }
+        }
+        messageCreateRequest = new MessageCreateRequest(
+                messageCreateRequest.content(),
+                messageCreateRequest.channelId(),
+                messageCreateRequest.authorId(),
+                binaryContentCreateRequests
+        );
+
+        Message message = messageService.create(messageCreateRequest);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(message);
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.PATCH)
-    @ResponseBody
-    public Message updateMessage(@PathVariable UUID id,
+    @GetMapping
+    public ResponseEntity<List<Message>> getAllMessages(@RequestParam UUID channelId) {
+        List<Message> messages = messageService.findAllByChannelId(channelId);
+        return ResponseEntity.ok(messages);
+
+    }
+
+    @PatchMapping("/{messageId}")
+    public ResponseEntity<Message> updateMessage(@PathVariable UUID messageId,
                                  @RequestBody MessageUpdateRequest messageUpdateRequest) {
-        return messageService.update(id,messageUpdateRequest);
+        Message message = messageService.update(messageId, messageUpdateRequest);
+        return ResponseEntity.ok(message);
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    @ResponseBody
-    public void deleteMessage(@PathVariable UUID id) {
-        messageService.delete(id);
+    @DeleteMapping("/{messageId}")
+    public ResponseEntity<Void> deleteMessage(@PathVariable UUID messageId) {
+        messageService.delete(messageId);
+        return ResponseEntity.noContent().build();//204
     }
 
-    @RequestMapping(method = RequestMethod.GET)
-    @ResponseBody
-    public List<Message> getAllMessages(@RequestParam UUID channelId) {
-        return messageService.findAllByChannelId(channelId);
 
-    }
 
 }
