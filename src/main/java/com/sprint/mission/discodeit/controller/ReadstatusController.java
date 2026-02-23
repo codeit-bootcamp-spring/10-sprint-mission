95199fe803617deedc33ebbc7d37a0eda9cdfdf2
jongin-git <jongin33@gmail.com>
2026-02-23 14:32:22 +0900

    fix: AuthController api 수정

--- src/main/java/com/sprint/mission/discodeit/controller/ReadstatusController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/ReadstatusController.java
@@ -7,7 +7,6 @@ import com.sprint.mission.discodeit.service.ReadStatusService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
