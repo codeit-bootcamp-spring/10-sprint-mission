c94167cdd0661e0ecf2662b5d54ad79d561dd350
dongg1122 <wkdehdrb0925@naver.com>
2026-02-24 14:05:31 +0900

    chore: swagger 설정을 위해 SwaggerConfig 추가

---
+++ src/main/java/com/sprint/mission/discodeit/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.sprint.mission.discodeit.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        Server localServer = new Server();
+        localServer.setUrl("http://localhost:8080");
+        localServer.setDescription("로컬 서버");
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Discodeit API 문서")
+                        .description("Discodeit 프로젝트의 Swagger API 문서입니다.")
+                )
+                .addServersItem(localServer);
+    }
+}
