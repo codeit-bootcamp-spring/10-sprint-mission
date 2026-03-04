6ebdd47d104064508a7fcd61da95f93e2c8ca8b3
jongin-git <jongin33@gmail.com>
2026-03-04 13:12:52 +0900

    feat: BaseEntity 추가

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -2,7 +2,10 @@ package com.sprint.mission.discodeit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+// @CreateDate 추가.
+@EnableJpaAuditing
 @SpringBootApplication
 public class DiscodeitApplication {
 
---
+++ src/main/java/com/sprint/mission/discodeit/entity/base/BaseUpdatableEntity.java
@@ -0,0 +1,21 @@
+package com.sprint.mission.discodeit.entity.base;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseUpdatableEntity {
+    @LastModifiedDate
+    @Column(nullable = false)
+    private Instant updatedAt;
+}
