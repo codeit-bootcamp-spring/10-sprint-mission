23462d7c5bebe873a7078ddb6ae1ba2a57c3aad3
kwakinsung <kis2690@gmail.com>
2026-03-25 16:45:26 +0900

    sprint7 베이스코드 setting

--- src/main/java/com/sprint/mission/discodeit/dto/request/UserStatusCreateRequest.java
+++
@@ -1,9 +0,0 @@
-package com.sprint.mission.discodeit.dto.request;
-
-import java.time.Instant;
-import java.util.UUID;
-
-public record UserStatusCreateRequest(
-        UUID userId,
-        Instant lastActiveAt //사용자가 마지막으로 활동한 시간. create인데 이게 필요한가?
-) {}
