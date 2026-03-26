0301ccf9cca639f0948bacb5fcac00a10c6b75c9
kwakinsung <kis2690@gmail.com>
2026-03-26 13:42:15 +0900

    chore: ChannelController 로깅 추가

--- src/main/java/com/sprint/mission/discodeit/controller/ChannelController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/ChannelController.java
@@ -9,6 +9,7 @@ import com.sprint.mission.discodeit.service.ChannelService;
 import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/channels")
@@ -30,7 +32,10 @@ public class ChannelController implements ChannelApi {
 
   @PostMapping(path = "public")
   public ResponseEntity<ChannelDto> create(@RequestBody PublicChannelCreateRequest request) {
+    log.debug("[CHANNEL_CREATE] PUBLIC 채널 생성 요청: channelName={}",request.name());
     ChannelDto createdChannel = channelService.create(request);
+
+    log.info("[CHANNEL_CREATE] PUBLIC 채널 생성 응답: channelId={},channelName={}",createdChannel.id(), request.name());
     return ResponseEntity
         .status(HttpStatus.CREATED)
         .body(createdChannel);
@@ -38,7 +43,10 @@ public class ChannelController implements ChannelApi {
 
   @PostMapping(path = "private")
   public ResponseEntity<ChannelDto> create(@RequestBody PrivateChannelCreateRequest request) {
+    log.debug("[CHANNEL_CREATE] PRIVATE 채널 생성 요청: participantCount={}",request.participantIds().size());
     ChannelDto createdChannel = channelService.create(request);
+
+    log.info("[CHANNEL_CREATE] PRIVATE 채널 생성 응답: channelId={},participantCount={}",createdChannel.id(), request.participantIds().size());
     return ResponseEntity
         .status(HttpStatus.CREATED)
         .body(createdChannel);
@@ -47,7 +55,11 @@ public class ChannelController implements ChannelApi {
   @PatchMapping(path = "{channelId}")
   public ResponseEntity<ChannelDto> update(@PathVariable("channelId") UUID channelId,
       @RequestBody PublicChannelUpdateRequest request) {
+
+    log.debug("[CHANNEL_UPDATE] 채널 수정 요청: channelId={}", channelId);
     ChannelDto updatedChannel = channelService.update(channelId, request);
+
+    log.info("[CHANNEL_UPDATE] 채널 수정 응답: channelId={}, channelName={}, channelDescription={}",channelId,updatedChannel.name(), updatedChannel.description());
     return ResponseEntity
         .status(HttpStatus.OK)
         .body(updatedChannel);
@@ -55,7 +67,10 @@ public class ChannelController implements ChannelApi {
 
   @DeleteMapping(path = "{channelId}")
   public ResponseEntity<Void> delete(@PathVariable("channelId") UUID channelId) {
+    log.debug("[CHANNEL_DELETE] 채널 삭제 요청: channelId={}", channelId);
     channelService.delete(channelId);
+
+    log.info("[CHANNEL_DELETE] 채널 삭제 응답: channelId={}", channelId);
     return ResponseEntity
         .status(HttpStatus.NO_CONTENT)
         .build();
