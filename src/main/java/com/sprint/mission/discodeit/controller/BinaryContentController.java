8b5a2ef6864da1e7006e4169ca538a4bc93b5e1f
kwakinsung <kis2690@gmail.com>
2026-03-26 13:45:01 +0900

    chore: BinaryContentlController 로깅 추가

--- src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
@@ -7,6 +7,7 @@ import com.sprint.mission.discodeit.storage.BinaryContentStorage;
 import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/binaryContents")
@@ -44,7 +46,10 @@ public class BinaryContentController implements BinaryContentApi {
   @GetMapping(path = "{binaryContentId}/download")
   public ResponseEntity<?> download(
       @PathVariable("binaryContentId") UUID binaryContentId) {
+    log.debug("[BINARYCONTENT_DOWNLOAD] 파일 다운로드 요청: binaryContentId={}", binaryContentId);
     BinaryContentDto binaryContentDto = binaryContentService.find(binaryContentId);
+
+    log.info("[BINARYCONTENT_DOWNLOAD] 파일 다운로드 응답: binaryContentId={}, fileName={}", binaryContentDto.id(), binaryContentDto.fileName());
     return binaryContentStorage.download(binaryContentDto);
   }
 }
