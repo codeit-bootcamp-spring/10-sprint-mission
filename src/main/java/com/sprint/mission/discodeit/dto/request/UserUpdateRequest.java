c3a619a4f4159532f764500fca7b051dcecf8246
kwakinsung <kis2690@gmail.com>
2026-03-27 11:50:02 +0900

    refactor: @Email 어노테이션 추가

--- src/main/java/com/sprint/mission/discodeit/dto/request/UserUpdateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/request/UserUpdateRequest.java
@@ -1,5 +1,6 @@
 package com.sprint.mission.discodeit.dto.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
 
 public record UserUpdateRequest(
@@ -8,6 +9,7 @@ public record UserUpdateRequest(
     String newUsername,
 
     @Size(max = 100)
+    @Email
     String newEmail,
 
     @Size(max = 60)
