d96857c18cc592900191802f0a5f4010e88d31bf
kwakinsung <kis2690@gmail.com>
2026-03-27 11:12:29 +0900

    refactor: DisCodeitException 핸들러 추가

--- src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
@@ -29,25 +29,29 @@ public class GlobalExceptionHandler {
   @ExceptionHandler(IllegalArgumentException.class)
   public ResponseEntity<String> handleException(IllegalArgumentException e) {
     e.printStackTrace();
-    return ResponseEntity
-        .status(HttpStatus.BAD_REQUEST)
-        .body(e.getMessage());
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
   }
 
   @ExceptionHandler(NoSuchElementException.class)
   public ResponseEntity<String> handleException(NoSuchElementException e) {
     e.printStackTrace();
-    return ResponseEntity
-        .status(HttpStatus.NOT_FOUND)
-        .body(e.getMessage());
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
   }
 
   @ExceptionHandler(Exception.class)
-  public ResponseEntity<String> handleException(Exception e) {
-    e.printStackTrace();
-    return ResponseEntity
-        .status(HttpStatus.INTERNAL_SERVER_ERROR)
-        .body(e.getMessage());
+  public ResponseEntity<ErrorResponse> handleException(Exception e) {
+    HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+    ErrorResponse error = new ErrorResponse((DiscodeitException) e, status.value());
+    return ResponseEntity.status(status).body(error);
+
+  }
+
+  @ExceptionHandler(DiscodeitException.class)
+  public ResponseEntity<ErrorResponse> handleException(DiscodeitException e) {
+    HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+    log.warn("[EXCEPTION] {}: {}", e.getClass().getSimpleName(), e.getMessage());
+    ErrorResponse error = new ErrorResponse((DiscodeitException) e, status.value());
+    return ResponseEntity.status(status).body(error);
   }
 
   @ExceptionHandler(UserNotFoundException.class)
