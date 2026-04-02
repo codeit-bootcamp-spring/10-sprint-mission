a776585c70ee53da44f906b2d11fac3a87e917d8
conradrado <gimmy6707@gmail.com>
2026-04-03 03:03:12 +0900

    feature: 각 컨트롤러 계층에서 WebMvcTest를 위한 Test 파일 생성

---
+++ src/test/java/com/sprint/mission/discodeit/repository/ChannelRepositoryTest.java
@@ -0,0 +1,20 @@
+package com.sprint.mission.discodeit.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ChannelRepositoryTest {
+
+    @Test
+    void existsByName() {
+    }
+
+    @Test
+    void findAllVisibleWithParticipants() {
+    }
+
+    @Test
+    void findWithParticipantsById() {
+    }
+}
