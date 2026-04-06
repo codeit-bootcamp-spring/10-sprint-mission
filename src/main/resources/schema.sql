a7a690932a8d43b9cd216160525e8fdea7a229c3
parkngg <millco0803com@gmail.com>
2026-04-06 17:02:15 +0900

    refactor: H2 호환성 개선을 위한 DB 스키마 데이터 타입 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS binary_contents (
     id UUID PRIMARY KEY,
-    created_at TIMESTAMPTZ NOT NULL,
+    created_at timestamp with time zone NOT NULL,
     file_name VARCHAR(255) NOT NULL,
     size BIGINT NOT NULL,
     content_type VARCHAR(100) NOT NULL
@@ -8,8 +8,8 @@ CREATE TABLE IF NOT EXISTS binary_contents (
 
 CREATE TABLE IF NOT EXISTS channels (
     id UUID PRIMARY KEY,
-    created_at TIMESTAMPTZ NOT NULL,
-    updated_at TIMESTAMPTZ,
+    created_at timestamp with time zone NOT NULL,
+    updated_at timestamp with time zone,
     name VARCHAR(100),
     description VARCHAR(500),
     type VARCHAR(10) NOT NULL
@@ -17,8 +17,8 @@ CREATE TABLE IF NOT EXISTS channels (
 
 CREATE TABLE IF NOT EXISTS users (
     id UUID PRIMARY KEY,
-    created_at TIMESTAMPTZ NOT NULL,
-    updated_at TIMESTAMPTZ,
+    created_at timestamp with time zone NOT NULL,
+    updated_at timestamp with time zone,
     username VARCHAR(50) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(60) NOT NULL,
@@ -28,18 +28,18 @@ CREATE TABLE IF NOT EXISTS users (
 
 CREATE TABLE IF NOT EXISTS user_statuses (
     id UUID PRIMARY KEY,
-    created_at TIMESTAMPTZ NOT NULL,
-    updated_at TIMESTAMPTZ,
+    created_at timestamp with time zone NOT NULL,
+    updated_at timestamp with time zone,
     user_id UUID NOT NULL UNIQUE,
-    last_active_at TIMESTAMPTZ NOT NULL,
+    last_active_at timestamp with time zone NOT NULL,
     CONSTRAINT fk_user_status_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
     );
 
 
 CREATE TABLE IF NOT EXISTS messages (
     id UUID PRIMARY KEY,
-    created_at TIMESTAMPTZ NOT NULL,
-    updated_at TIMESTAMPTZ,
+    created_at timestamp with time zone NOT NULL,
+    updated_at timestamp with time zone,
     content TEXT,
     channel_id UUID NOT NULL,
     author_id UUID,
@@ -49,11 +49,11 @@ CREATE TABLE IF NOT EXISTS messages (
 
 CREATE TABLE IF NOT EXISTS read_statuses (
     id UUID PRIMARY KEY,
-    created_at TIMESTAMPTZ NOT NULL,
-    updated_at TIMESTAMPTZ,
+    created_at timestamp with time zone NOT NULL,
+    updated_at timestamp with time zone,
     user_id UUID NOT NULL,
     channel_id UUID NOT NULL,
-    last_read_at TIMESTAMPTZ NOT NULL,
+    last_read_at timestamp with time zone NOT NULL,
     CONSTRAINT fk_read_status_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
     CONSTRAINT fk_read_status_channel FOREIGN KEY (channel_id) REFERENCES channels(id) ON DELETE CASCADE,
     UNIQUE (user_id, channel_id)
