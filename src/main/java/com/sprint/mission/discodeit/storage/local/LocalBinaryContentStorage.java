8d58d81a850319f34d1179235a1be14592b8aa23
kwakinsung <kis2690@gmail.com>
2026-04-08 09:14:27 +0900

    feat: AWS S3 테스트

--- src/main/java/com/sprint/mission/discodeit/storage/local/LocalBinaryContentStorage.java
+++ src/main/java/com/sprint/mission/discodeit/storage/local/LocalBinaryContentStorage.java
@@ -26,7 +26,7 @@ public class LocalBinaryContentStorage implements BinaryContentStorage {
   private final Path root;
 
   public LocalBinaryContentStorage(
-      @Value("${discodeit.storage.local.root-path}") Path root
+      @Value("${discodeit.storage.local.root-path}") Path root  //app/storage
   ) {
     this.root = root;
   }
@@ -43,7 +43,7 @@ public class LocalBinaryContentStorage implements BinaryContentStorage {
     }
   }
 
-  public UUID put(UUID binaryContentId, byte[] bytes) {
+  public UUID put(UUID binaryContentId, byte[] bytes, String contentType) {
     Path filePath = resolvePath(binaryContentId);
     if (Files.exists(filePath)) {
       throw new IllegalArgumentException("File with key " + binaryContentId + " already exists");
