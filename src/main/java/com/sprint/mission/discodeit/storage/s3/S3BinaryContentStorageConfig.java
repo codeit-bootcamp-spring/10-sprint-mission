1220ec8c34b9b5a22054d4851b732f09cf6b3e09
jongin-git <jongin33@gmail.com>
2026-04-08 11:23:31 +0900

    feat: S3Binary 고도화 및 ㅌ애플리케이션 테스트 완료

---
+++ src/main/java/com/sprint/mission/discodeit/storage/s3/S3BinaryContentStorageConfig.java
@@ -0,0 +1,24 @@
+package com.sprint.mission.discodeit.storage.s3;
+
+import com.sprint.mission.discodeit.storage.BinaryContentStorage;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(S3StorageProperties.class)
+public class S3BinaryContentStorageConfig {
+
+    @Bean
+    @ConditionalOnProperty(name = "discodeit.storage.type", havingValue = "s3")
+    public BinaryContentStorage s3BinaryContentStorage() {
+        return new S3BinaryContentStorage(
+                System.getenv("AWS_S3_ACCESS_KEY"),
+                System.getenv("AWS_S3_SECRET_KEY"),
+                System.getenv("AWS_S3_REGION"),
+                System.getenv("AWS_S3_BUCKET"),
+                Long.parseLong(System.getenv().getOrDefault("AWS_S3_PRESIGNED_URL_EXPIRATION", "600"))
+        );
+    }
+}
