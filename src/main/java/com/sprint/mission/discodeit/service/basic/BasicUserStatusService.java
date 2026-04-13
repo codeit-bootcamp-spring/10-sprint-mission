ccbb526a32534038270aacfae04817794a659e09
kwakinsung <kis2690@gmail.com>
2026-04-13 17:43:57 +0900

    refactor: @PastOrPresent 어노테이션 일시제거

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserStatusService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserStatusService.java
@@ -75,7 +75,7 @@ public class BasicUserStatusService implements UserStatusService {
   @Transactional
   @Override
   public UserStatusDto update(UUID userStatusId, UserStatusUpdateRequest request) {
-    Instant newLastActiveAt = Instant.now();
+    Instant newLastActiveAt = request.newLastActiveAt();
     log.debug("사용자 상태 수정 시작: id={}, newLastActiveAt={}", 
         userStatusId, newLastActiveAt);
     
@@ -90,7 +90,7 @@ public class BasicUserStatusService implements UserStatusService {
   @Transactional
   @Override
   public UserStatusDto updateByUserId(UUID userId, UserStatusUpdateRequest request) {
-    Instant newLastActiveAt = Instant.now();
+    Instant newLastActiveAt = request.newLastActiveAt();
     log.debug("사용자 ID로 상태 수정 시작: userId={}, newLastActiveAt={}", userId, newLastActiveAt);
     
     UserStatus userStatus = userStatusRepository.findByUserId(userId)
