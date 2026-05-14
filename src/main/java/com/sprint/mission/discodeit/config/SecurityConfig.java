5bc04cc8aa08f1722873342058360192a66e019e
conradrado <gimmy6707@gmail.com>
2026-05-14 11:07:39 +0900

    Feat: CSRF 보호 설정 - SecurityConfig 필터 체인을 설정   - CsrfTokenRepository 구현체를 CookieCsrfTokenRepository로 설정   - CsrfTokenRequestHandler를 SpaCsrfTokenRequestHandler로 설정 - SpaCsrfTokenRequestHandler 작성. - CsrfController 작성   - /csrf-token 엔드포인트로 GET 전달 시 203 상태 반환 및 CSRF 발급

--- src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 public class SecurityConfig {
@@ -12,8 +13,10 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     return http
-        .csrf(csrf -> csrf.disable())
-        .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+        .csrf(csrf -> csrf
+            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+            .csrfTokenRequestHandler(new SpaCsrfTokenRequestHandler())
+        )
         .build();
   }
 }
---
+++ src/main/java/com/sprint/mission/discodeit/controller/CsrfController.java
@@ -0,0 +1,23 @@
+package com.sprint.mission.discodeit.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/csrf-token")
+public class CsrfController {
+
+  @GetMapping
+  public ResponseEntity<Void> getCsrfToken(CsrfToken csrfToken) {
+    String tokenValue = csrfToken.getToken();
+    log.debug("CSRF 토큰 요청: {}", tokenValue);
+
+    return ResponseEntity.status(HttpStatus.NON_AUTHORITATIVE_INFORMATION).build();
+  }
+}
