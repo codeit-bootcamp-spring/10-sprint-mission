fafbf5b5d35f7784d8440f257b7857d05c82e6b9
plzslp <gyutann1171@gmail.com>
2026-05-15 11:49:30 +0900

    refactor: 기존 로그인 컨트롤러, 서비스 로직 삭제

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -1,22 +1,12 @@
 package com.sprint.mission.discodeit.controller;
 
-import com.sprint.mission.discodeit.dto.user.LoginRequest;
-import com.sprint.mission.discodeit.dto.user.UserDto;
-import com.sprint.mission.discodeit.service.AuthService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,23 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Auth")
 @Slf4j
 public class AuthController {
-
-  private final AuthService authService;
-
-  @PostMapping("/login")
-  @Operation(summary = "로그인")
-  @ApiResponses({
-      @ApiResponse(responseCode = "200", description = "로그인 성공"),
-      @ApiResponse(responseCode = "404", description = "사용자를 찾을 수 없음"),
-      @ApiResponse(responseCode = "400", description = "비밀번호가 일치하지 않음")
-  })
-  public ResponseEntity<UserDto> login(
-      @Valid @RequestBody LoginRequest request
-  ) {
-    log.info("유저 로그인 요청: username={}", request.username());
-    UserDto response = authService.login(request);
-    return ResponseEntity.status(HttpStatus.OK).body(response);
-  }
 
   @GetMapping("csrf-token")
   public ResponseEntity<Void> getCsrfToken(CsrfToken csrfToken) {
