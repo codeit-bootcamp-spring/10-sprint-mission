bf8aca72296126945a05af82f139dd54f431989d
jongin-git <jongin33@gmail.com>
2026-05-15 10:36:22 +0900

    feat: 세션 사용해서 로그인 정보 유지 확인

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -1,10 +1,12 @@
 package com.sprint.mission.discodeit.controller;
 
-import com.sprint.mission.discodeit.service.AuthService;
+import com.sprint.mission.discodeit.dto.data.UserDto;
+import com.sprint.mission.discodeit.security.DiscodeitUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,8 +19,6 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/api/auth")
 public class AuthController {
 
-  private final AuthService authService;
-
   @GetMapping("/csrf-token")
   public ResponseEntity<Void> csrfToken(CsrfToken csrfToken) {
     // csrfToken.getToken()을 호출하면 토큰이 실제로 생성
@@ -29,5 +29,17 @@ public class AuthController {
 
     // 응답 203 , body X
     return ResponseEntity.status(HttpStatus.NON_AUTHORITATIVE_INFORMATION).build();
+  }
+
+  @GetMapping("/me")
+  public ResponseEntity<UserDto> me(@AuthenticationPrincipal DiscodeitUserDetails userDetails) {
+    if (userDetails == null) {
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+    UserDto currentUser = userDetails.getUserDto();
+
+    log.debug("현재 사용자 정보 조회: userId={}, username={}", currentUser.id(), currentUser.username());
+
+    return ResponseEntity.status(HttpStatus.OK).body(currentUser);
   }
 }
--- src/main/java/com/sprint/mission/discodeit/service/AuthService.java
+++ src/main/java/com/sprint/mission/discodeit/service/AuthService.java
@@ -1,9 +1,9 @@
-package com.sprint.mission.discodeit.service;
-
-import com.sprint.mission.discodeit.dto.data.UserDto;
-import com.sprint.mission.discodeit.dto.request.LoginRequest;
-
-public interface AuthService {
-
-  UserDto login(LoginRequest loginRequest);
-}
+//package com.sprint.mission.discodeit.service;
+//
+//import com.sprint.mission.discodeit.dto.data.UserDto;
+//
+//
+//public interface AuthService {
+//
+//  UserDto login(LoginRequest loginRequest);
+//}
