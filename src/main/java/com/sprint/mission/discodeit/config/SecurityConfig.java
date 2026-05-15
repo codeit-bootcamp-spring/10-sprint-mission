9c227a0e1fa45a01f5aabf1820599527707da360
parkngg <millco0803com@gmail.com>
2026-05-15 18:03:19 +0900

    feat: Spring Security 기반 RESTful 로그아웃 처리 구현

--- src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
@@ -5,11 +5,13 @@ import com.sprint.mission.discodeit.security.LoginSuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
@@ -28,19 +30,28 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
+        // CSRF 보호 설정
         .csrf(csrf -> csrf
             // 프론트엔드(JS)에서 쿠키를 읽을 수 있도록 HttpOnly 설정을 false로 변경
             .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
             // SPA 전용 핸들러 등록
             .csrfTokenRequestHandler(new SpaCsrfTokenRequestHandler())
         )
+        // 인가 설정
         .authorizeHttpRequests(auth -> auth
             .anyRequest().permitAll()
         )
+        // 로그인 설정
         .formLogin(login -> login
             .loginProcessingUrl("/api/auth/login") // Security 필터가 낚아챌 로그인 URL
             .successHandler(loginSuccessHandler)   // 200 응답 핸들러
             .failureHandler(loginFailureHandler)   // 401 응답 핸들러
+        )
+        // 로그아웃 설정
+        .logout(logout -> logout
+            .logoutUrl("/api/auth/logout") // Security 필터가 낚아챌 로그아웃 URL
+            .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(
+                HttpStatus.NO_CONTENT)) // 302 리다이렉트 대신 204 응답 반환
         );
     return http.build();
   }
