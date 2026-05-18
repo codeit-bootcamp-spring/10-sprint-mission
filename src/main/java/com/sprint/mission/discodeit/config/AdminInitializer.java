03ae107d1aeb3005296bf61dc94aee236e5f6f4f
parkngg <millco0803com@gmail.com>
2026-05-18 14:44:21 +0900

    feat: 애플리케이션 기동 시 기본 ADMIN 계정 초기화 로직 추가

---
+++ src/main/java/com/sprint/mission/discodeit/config/AdminInitializer.java
@@ -0,0 +1,40 @@
+package com.sprint.mission.discodeit.config;
+
+import com.sprint.mission.discodeit.entity.Role;
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.entity.UserStatus;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import java.time.Instant;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class AdminInitializer implements ApplicationRunner {
+
+  private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
+
+  @Override
+  @Transactional
+  public void run(ApplicationArguments args) {
+    // 어드민 계정이 없을 때만 생성
+    if (!userRepository.existsByUsername("admin")) {
+      log.info("ADMIN account not found. Proceeding with initialization.");
+      User admin = new User("admin", "admin@admin.com", passwordEncoder.encode("admin1234!"), null);
+      admin.updateRole(Role.ADMIN);
+
+      UserStatus status = new UserStatus(admin, Instant.now());
+      admin.assignUserStatus(status);
+
+      userRepository.save(admin);
+      log.info("ADMIN account created successfully.");
+    }
+  }
+}
