09b93cdb301810032f4166ffe6250b54ade12fa5
jh9dev <sungjuhyun1001@gmail.com>
2026-05-21 11:28:46 +0900

    test: 사용자 삭제 테스트에 SessionRegistry mock 추가

--- src/test/java/com/sprint/mission/discodeit/service/basic/BasicUserServiceTest.java
+++ src/test/java/com/sprint/mission/discodeit/service/basic/BasicUserServiceTest.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.util.ReflectionTestUtils;
 
@@ -38,6 +39,9 @@ class BasicUserServiceTest {
 
 	@Mock
 	private PasswordEncoder passwordEncoder;
+
+	@Mock
+	private SessionRegistry sessionRegistry;
 
 	@InjectMocks
 	private BasicUserService userService;
