b2c43807b735b7864d49f98086d2be36f788eb32
zelote123 <fredsonata@gmail.com>
2026-05-26 14:31:20 +0900

    fix: userstatus 관련 제거

--- src/main/java/com/sprint/mission/discodeit/service/UserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/UserService.java
@@ -1,10 +1,8 @@
 package com.sprint.mission.discodeit.service;
 
-import com.sprint.mission.discodeit.dto.authDto.UserRoleUpdateRequest;
 import com.sprint.mission.discodeit.dto.user.UserCreateRequest;
 import com.sprint.mission.discodeit.dto.user.UserDto;
 import com.sprint.mission.discodeit.dto.user.UserUpdateRequest;
-import com.sprint.mission.discodeit.dto.userStatus.UserStatusUpdateRequest;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
