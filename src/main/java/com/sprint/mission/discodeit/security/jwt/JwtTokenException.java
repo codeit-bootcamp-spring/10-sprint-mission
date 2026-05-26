75ce152adebc8d22f545d01727c762d71674d98f
jh9dev <sungjuhyun1001@gmail.com>
2026-05-26 11:06:31 +0900

    feat: JWT 토큰 발급 및 검증 컴포넌트 구현

---
+++ src/main/java/com/sprint/mission/discodeit/security/jwt/JwtTokenException.java
@@ -0,0 +1,12 @@
+package com.sprint.mission.discodeit.security.jwt;
+
+public class JwtTokenException extends RuntimeException {
+
+	public JwtTokenException(String message) {
+		super(message);
+	}
+
+	public JwtTokenException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
