c51ef74c2d74504a6379c0abfcf8ee82a92972b7
conradrado <gimmy6707@gmail.com>
2026-06-01 16:26:16 +0900

    BinaryContent 엔티티 리팩토링 - BinaryContentStatus enum 작성 - BinaryContent는 status를 가짐 - BinaryContent는 BaseUpdatableEntity를 구현 - schema.sql 내 BinaryContent 테이블 컬럼 추가

--- src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
+++ src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
@@ -1,6 +1,8 @@
 package com.sprint.mission.discodeit.entity;
 
 import com.sprint.mission.discodeit.entity.base.BaseEntity;
+import com.sprint.mission.discodeit.entity.base.BaseUpdatableEntity;
+import com.sprint.mission.discodeit.enums.binarycontents.BinaryContentStatus;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
@@ -11,32 +13,39 @@ import lombok.Setter;
 @Setter
 @Entity
 @Table(name = "binary_contents")
-public class BinaryContent extends BaseEntity {
+public class BinaryContent extends BaseUpdatableEntity {
 
-    @Column(nullable = false)
-    private String fileName;
+  @Column(nullable = false)
+  private String fileName;
 
-    @Column(nullable = false)
-    private Long size;
+  @Column(nullable = false)
+  private Long size;
 
-    @Column(nullable = false)
-    private String contentType;
+  @Column(nullable = false)
+  private String contentType;
 
-    @Column(nullable = false, columnDefinition = "bytea")
-    private byte[] bytes = new byte[0];
+  @Column(nullable = false, columnDefinition = "bytea")
+  private byte[] bytes = new byte[0];
 
-    public BinaryContent(String fileName, Long size, String contentType, byte[] bytes) {
-        this.fileName = fileName;
-        this.size = size;
-        this.contentType = contentType;
-        this.bytes = bytes;
-    }
+  @Column(nullable = false)
+  private BinaryContentStatus status;
 
-    public BinaryContent(String fileName, Long size, String contentType) {
-        this(fileName, size, contentType, new byte[0]);
-    }
+  public BinaryContent(String fileName, Long size, String contentType, byte[] bytes) {
+    this.fileName = fileName;
+    this.size = size;
+    this.contentType = contentType;
+    this.bytes = bytes;
+  }
 
-    public BinaryContent() {
+  public BinaryContent(String fileName, Long size, String contentType) {
+    this(fileName, size, contentType, new byte[0]);
+  }
 
-    }
+  public BinaryContent() {
+
+  }
+
+  public void updateStatus(BinaryContentStatus status) {
+    this.status = status;
+  }
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -45,10 +45,12 @@ CREATE TABLE binary_contents
 (
     id           UUID PRIMARY KEY,
     created_at   timestamptz  NOT NULL,
+    updated_at   timestamptz  NOT NULL,
     file_name    VARCHAR(255) NOT NULL,
     size         BIGINT       NOT NULL,
     content_type VARCHAR(100) NOT NULL,
-    bytes        BYTEA        NOT NULL
+    bytes        BYTEA        NOT NULL,
+    status       varchar(20)  NOT NULL
 );
 
 ALTER TABLE users
