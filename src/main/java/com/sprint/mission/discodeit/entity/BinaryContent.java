5a3617558fc62cd3a2755f4a8fbcfd8e06a98f1e
parkngg <millco0803com@gmail.com>
2026-06-02 15:37:14 +0900

    feat: BinaryContent 업로드 상태 관리를 위해 status 필드 추가

--- src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
+++ src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
@@ -1,8 +1,10 @@
 package com.sprint.mission.discodeit.entity;
 
-import com.sprint.mission.discodeit.entity.base.BaseEntity;
+import com.sprint.mission.discodeit.entity.base.BaseUpdatableEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -14,7 +16,7 @@ import lombok.ToString;
 @Getter
 @ToString(callSuper = true)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class BinaryContent extends BaseEntity {
+public class BinaryContent extends BaseUpdatableEntity {
 
   @Column(nullable = false)
   private String fileName;
@@ -25,10 +27,19 @@ public class BinaryContent extends BaseEntity {
   @Column(nullable = false)
   private String contentType;
 
+  // 바이너리 데이터 저장 성공 여부를 알 수 있는 상태 필드
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false, length = 20)
+  private BinaryContentStatus status = BinaryContentStatus.PROCESSING; // 기본값 PROCESSING
+
   public BinaryContent(String fileName, long size, String contentType) {
     super();
     this.fileName = fileName;
     this.size = size;
     this.contentType = contentType;
+  }
+
+  public void updateStatus(BinaryContentStatus status) {
+    this.status = status;
   }
 }
