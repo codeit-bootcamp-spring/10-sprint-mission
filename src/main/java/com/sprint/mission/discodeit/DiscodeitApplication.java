87ebd96a52fbd8d50cd2b3b9a57a039b387315ff
shong9124 <shifty393@gmail.com>
2026-06-09 11:39:25 +0900

    feat: redis 적용

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -2,10 +2,8 @@ package com.sprint.mission.discodeit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ConfigurableApplicationContext;
 
-@EnableCaching
 @SpringBootApplication
 public class DiscodeitApplication {
 
