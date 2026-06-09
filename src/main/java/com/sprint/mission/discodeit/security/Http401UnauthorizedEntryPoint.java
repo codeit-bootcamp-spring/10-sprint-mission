2b84edc5948846f8bf07fc94ae84bd36437f1726
jongin-git <jongin33@gmail.com>
2026-06-09 10:24:29 +0900

    feat: 예외 401/403 분리 및 적용

---
+++ src/main/java/com/sprint/mission/discodeit/security/Http401UnauthorizedEntryPoint.java
@@ -0,0 +1,33 @@
+package com.sprint.mission.discodeit.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sprint.mission.discodeit.exception.ErrorResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+@RequiredArgsConstructor
+public class Http401UnauthorizedEntryPoint implements AuthenticationEntryPoint {
+
+  private final ObjectMapper objectMapper;
+
+  @Override
+  public void commence(
+      HttpServletRequest request,
+      HttpServletResponse response,
+      AuthenticationException authException)
+      throws IOException, ServletException {
+    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+    response.setContentType("application/json");
+    response.setCharacterEncoding("UTF-8");
+
+    ErrorResponse errorResponse =
+        new ErrorResponse(authException, HttpServletResponse.SC_UNAUTHORIZED);
+
+    response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+  }
+}
