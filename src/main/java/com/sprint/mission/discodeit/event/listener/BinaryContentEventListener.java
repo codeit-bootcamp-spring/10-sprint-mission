59d5869b197c27d4836cad85887ba8d85c29a758
parkngg <millco0803com@gmail.com>
2026-06-12 04:55:04 +0900

    refactor: 첨부파일 업로드 비동기 이벤트 처리 적용

--- src/main/java/com/sprint/mission/discodeit/event/listener/BinaryContentEventListener.java
+++ src/main/java/com/sprint/mission/discodeit/event/listener/BinaryContentEventListener.java
@@ -1,12 +1,12 @@
 package com.sprint.mission.discodeit.event.listener;
 
-import com.sprint.mission.discodeit.entity.BinaryContent;
 import com.sprint.mission.discodeit.entity.BinaryContentStatus;
 import com.sprint.mission.discodeit.event.BinaryContentCreatedEvent;
 import com.sprint.mission.discodeit.service.BinaryContentService;
 import com.sprint.mission.discodeit.storage.BinaryContentStorage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,6 +21,7 @@ public class BinaryContentEventListener {
   private final BinaryContentStorage binaryContentStorage;
   private final BinaryContentService binaryContentService;
 
+  @Async("eventTaskExecutor") // 비동기 처리
   // 서비스 메인 트랜잭션이 커밋된 후(DB에 메타데이터가 저장된 후)에만 리스너가 실행되도록 설정
   @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
   // 메인 트랜잭션은 이미 끝났으므로, 상태값 업데이트를 DB에 반영하려면 새 트랜잭션을 열어야 함
