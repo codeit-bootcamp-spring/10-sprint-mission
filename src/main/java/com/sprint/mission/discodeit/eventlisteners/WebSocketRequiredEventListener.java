94eb53ec098b837cb11060e2b507325ead338284
conradrado <gimmy6707@gmail.com>
2026-06-15 11:03:06 +0900

    Feat: 메시지 송수신 기능 구현 - 첨부파일이 없는 메시지 전송 시 STOMP - 첨부파일이 있는 메시지 전송 시 REST-API로

---
+++ src/main/java/com/sprint/mission/discodeit/eventlisteners/WebSocketRequiredEventListener.java
@@ -0,0 +1,26 @@
+package com.sprint.mission.discodeit.eventlisteners;
+
+import com.sprint.mission.discodeit.dto.messagedto.MessageDto;
+import com.sprint.mission.discodeit.events.MessageCreatedEvent;
+import com.sprint.mission.discodeit.service.MessageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Component
+@RequiredArgsConstructor
+public class WebSocketRequiredEventListener {
+
+  private final SimpMessagingTemplate messagingTemplate;
+  private final MessageService messageService;
+
+  @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+  public void handleMessage(MessageCreatedEvent event) {
+    MessageDto message = messageService.find(event.messageId());
+
+    messagingTemplate.convertAndSend("/sub/channels." + message.channelId() + ".messages",
+        message);
+  }
+}
