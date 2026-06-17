0fae4869a566a4e25f8aa3baf2334905135f7f2b
zelote123 <fredsonata@gmail.com>
2026-06-17 18:34:03 +0900

    fix: Sse요청 시 인증된 사용자만 허가하도록 수정

--- src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
@@ -70,7 +70,6 @@ public class SecurityConfig {
                                 AntPathRequestMatcher.antMatcher(HttpMethod.POST, "/api/auth/login"),
                                 AntPathRequestMatcher.antMatcher(HttpMethod.POST, "/api/auth/refresh"),
                                 AntPathRequestMatcher.antMatcher(HttpMethod.POST, "/api/auth/logout"),
-                                AntPathRequestMatcher.antMatcher(HttpMethod.GET, "/api/sse"),
                                 new NegatedRequestMatcher(AntPathRequestMatcher.antMatcher("/api/**"))
                         ).permitAll()
                         .anyRequest().authenticated())
