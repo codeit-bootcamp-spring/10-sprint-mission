bb0be6501722cd9f18558c7c19cfc27ae95f5d73
kwakinsung <kis2690@gmail.com>
2026-06-18 14:17:30 +0900

    refactor:Websocket 보안적용

--- src/main/java/com/sprint/mission/discodeit/config/WebSocketConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/WebSocketConfig.java
@@ -1,10 +1,16 @@
 package com.sprint.mission.discodeit.config;
 
+import com.sprint.mission.discodeit.entity.Role;
+import com.sprint.mission.discodeit.security.JwtAuthenticationChannelInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
+import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.messaging.access.intercept.AuthorizationChannelInterceptor;
+import org.springframework.security.messaging.access.intercept.MessageMatcherDelegatingAuthorizationManager;
+import org.springframework.security.messaging.context.SecurityContextChannelInterceptor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -13,7 +19,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker   //STOMP기반 WebSocket 메시징 활성화
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-//    private final StompChannelInterceptor interceptor;
+    private final JwtAuthenticationChannelInterceptor jwtAuthenticationChannelInterceptor;
 
     /// WebSocket 최초 연결 주소 설정.
     /// 클라이언트가 GET /ws-stomp 요청을 보내고, 요청 헤더에 WebSocket으로 업그레이드하고 싶다는 정보 있으면
@@ -51,10 +57,39 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         config.enableSimpleBroker("/sub");
     }
 
-//    @Override
-//    public void configureClientInboundChannel(ChannelRegistration registry) {
-//        registry.interceptors(interceptor);
-//    }
+    /// 클라이언트 -> 서버로 들어오는 STOMP 메시지 채널에 인터셉터를 등록하는 설정
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+
+        /// CONNECT 요청시,
+        /// (1) jwtAuthenticationChannelInterceptor: JWT 검증
+        /// (2) SecurityContextChannelInterceptor
+        /// 를 거쳐서 Controller로 향한다.
+        registration.interceptors(
+                jwtAuthenticationChannelInterceptor,
+                /// SecurityContextHoler는 보통 현재 요청을 처리하는 스레드에만 저장된다.
+                /// 로그인 HTTP요청에서 인증됐다고 해서 나중에 WebSocket 메시지를 처리하는 스레드까지 그 인증 정보가 자동으로 남아 있지는 않는다.
+                /// jwtAuthenticationChannelInterceptor에서 반환된 인증객체를 Spring SecurityContex에 연결한다.
+                new SecurityContextChannelInterceptor(),
+                authorizationChannelInterceptor()
+                );
+    }
+
+    /// 모든 inbound STOMP 메시지에 USER 역할을 요구.
+    private AuthorizationChannelInterceptor authorizationChannelInterceptor() {
+        /// AuthorizationManger: Spring Security의 인가 판단 객체
+        AuthorizationManager<Message<?>> authorizationManager =
+                MessageMatcherDelegatingAuthorizationManager.builder()
+                        /// CONNECT,SUBSCRIBE, SEND, DISCONNECT 모두 규칙적용.
+                        .anyMessage()
+                        /// hasRole은 내부적으로 ROLE_ 접두사를 붙여 권한을 검사한다.
+                        /// 사용자의 권한지 ROLE_USER라면 허용되고, 인증정보 없거나 필요한 역할 없다면
+                        /// AuthorizationChannelInterceptor가 메시지 처리 거부.
+                        .hasRole(Role.USER.name())
+                        .build();
+
+        return new AuthorizationChannelInterceptor(authorizationManager);
+    }
 
 
 
--- src/main/java/com/sprint/mission/discodeit/controller/MessageWebSocketController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/MessageWebSocketController.java
@@ -8,6 +8,7 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Controller;
 
+import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -15,9 +16,11 @@ import java.util.List;
 public class MessageWebSocketController {
     private final BasicMessageService messageService;
 
+    /// HTTP 요청 처리가 아닌 SEND STOMP 메시지 처리.
     /// @Payload: STOMP 메시지 바디를 이 파라미터에 매핑해줘.
     @MessageMapping("/messages") //클라이언트의 /pub/messages 요청 처리
-    public void sendMessage(@Valid @Payload MessageCreateRequest request) {
+    public void sendMessage(@Valid @Payload MessageCreateRequest request,
+                            Principal principal) {
         messageService.create(request, List.of());
     }
 }
