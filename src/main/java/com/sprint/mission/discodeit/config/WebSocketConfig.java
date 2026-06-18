60d119765522c6144775486bd04c013044eba0db
zelote123 <fredsonata@gmail.com>
2026-06-18 11:43:20 +0900

    feat: 웹소켓 인증/인가 구현

--- src/main/java/com/sprint/mission/discodeit/config/WebSocketConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/WebSocketConfig.java
@@ -1,15 +1,23 @@
 package com.sprint.mission.discodeit.config;
 
-import org.springframework.context.annotation.Bean;
+import com.sprint.mission.discodeit.entity.enums.Role;
+import com.sprint.mission.discodeit.interceptor.JwtAuthenticationInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.security.messaging.access.intercept.AuthorizationChannelInterceptor;
+import org.springframework.security.messaging.access.intercept.MessageMatcherDelegatingAuthorizationManager;
+import org.springframework.security.messaging.context.SecurityContextChannelInterceptor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @EnableWebSocketMessageBroker
 @Configuration
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    private final JwtAuthenticationInterceptor jwtAuthenticationInterceptor;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -25,5 +33,20 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
         // 구독 경로
         registry.enableSimpleBroker("/sub");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(jwtAuthenticationInterceptor,
+                new SecurityContextChannelInterceptor(),
+                authorizationChannelInterceptor());
+    }
+
+    private AuthorizationChannelInterceptor authorizationChannelInterceptor(){
+        return new AuthorizationChannelInterceptor(
+                MessageMatcherDelegatingAuthorizationManager.builder()
+                        .anyMessage().hasRole(Role.USER.name())
+                        .build()
+        );
     }
 }
---
+++ src/main/java/com/sprint/mission/discodeit/interceptor/JwtAuthenticationInterceptor.java
@@ -0,0 +1,42 @@
+package com.sprint.mission.discodeit.interceptor;
+
+import com.sprint.mission.discodeit.auth.jwt.JwtTokenProvider;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.Nullable;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationInterceptor implements ChannelInterceptor {
+    private final JwtTokenProvider  jwtTokenProvider;
+
+    @Override
+    public @Nullable Message<?> preSend(Message<?> message, MessageChannel channel) {
+        StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+        if(StompCommand.CONNECT.equals(accessor.getCommand())){
+            String token = accessor.getFirstNativeHeader("Authorization");
+            if(!jwtTokenProvider.validateToken(resolveToken(token))){
+                throw new IllegalArgumentException("Invalid Token");
+            }
+            UsernamePasswordAuthenticationToken authentication = (UsernamePasswordAuthenticationToken) jwtTokenProvider.getAuthentication(token);
+            accessor.setUser(authentication);
+        }
+        return message;
+    }
+
+    private String resolveToken(String bearerToken){
+        if(StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")){
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+}
